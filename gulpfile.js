d08654d891c9c67d85bc182be5d61d92cf7350c0
Min RK <benjaminrk@gmail.com>
2015-04-22 11:56:57 -0700

    get widget css building with gulp css
    load upstream less from jupyter_notebook
    
    node 0.10 doesn't have spawnSync (really?)

--- gulpfile.js
+++ gulpfile.js
@@ -4,18 +4,26 @@ var path = require('path');
 var minifyCSS = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sourcemaps = require('gulp-sourcemaps');
- 
-gulp.task('css', function () {
-  return gulp.src('./jupyter_notebook/static/style/*.less')
+
+// spawn-sync polyfill for nodejs < 0.12
+var spawn = require('spawn-sync');
+// var spawn = require('child_process').spawnSync;
+
+
+gulp.task('css', function (cb) {
+  var p = spawn('python', ['-c',
+    "import os,jupyter_notebook; print(os.path.join(jupyter_notebook.DEFAULT_STATIC_FILES_PATH))"]);
+  var nb_static_path = p.stdout.toString().trim();
+  return gulp.src('./ipython_widgets/static/widgets/css/widgets.less')
     .pipe(sourcemaps.init())
     .pipe(less({
-      paths: [ path.join(__dirname, 'less', 'includes') ]
+      paths: [nb_static_path],
     }))
     .pipe(minifyCSS())
     .pipe(rename({
+            dirname: path.join('..', 'css'),
             suffix: '.min'
         }))
     .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./jupyter_notebook/static/style'));
+    .pipe(gulp.dest('./ipython_widgets/static/widgets/css/'));
 });
-
