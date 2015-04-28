61113a9d2fcfedcf1447f3fbf724248fb785a0fb
Jonathan Frederic <jon.freder@gmail.com>
2015-04-28 16:54:44 -0700

    Register a widget comm target in the widgets, not ipython_kernel.

--- ipython_widgets/__init__.py
+++ ipython_widgets/__init__.py
@@ -1,1 +1,5 @@
 from .widgets import *
+
+# Register comm target for Javascript initialized widgets..
+ip = get_ipython()
+ip.kernel.comm_manager.register_target('ipython.widget', Widget.handle_comm_opened)
