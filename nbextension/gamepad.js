bc54565e0464b24ca989a7a135e52737f83680a3
Sylvain Corlay <sylvain.corlay@gmail.com>
2015-06-20 01:28:01 -0400

    Improve layout and add axis/button indices.

--- nbextension/gamepad.js
+++ nbextension/gamepad.js
@@ -8,28 +8,31 @@ define([
     var Button = widget.DOMWidgetView.extend({
         /* Very simple view for a gamepad button. */
         render : function(){
-            this.$el.css({
+            this.$support = $('<div />').css({
                     'position': 'relative',
                     'margin': '1px',
-                    'min-width': '16px',
-                    'min-height': '16px',
+                    'width': '16px',
+                    'height': '16px',
                     'border': '1px solid black',
-                    'border-radius': '5px',
-                    'background-color': 'lightgray',
-                });
+                    'background': 'lightgray',
+                })
+                .appendTo(this.$el);
             this.$bar = $('<div />')
                 .css({
                     'position': 'absolute',
                     'width': '100%',
                     'bottom': 0,
-                    'background-color': 'gray',
-                    'border-radius': '5px',
+                    'background': 'gray',
                 })
+                .appendTo(this.$support);
+            this.$label = $('<div />')
+                .text(this.model.index)
+                .css('text-align', 'center')
                 .appendTo(this.$el);
             this.update();
         },
 
-        update : function(){
+        update : function() {
             this.$bar.css('height', 100 * this.model.get('value') + '%');
         },
     });
@@ -37,29 +40,32 @@ define([
     var Axis = widget.DOMWidgetView.extend({
         /* Very simple view for a gamepad axis. */
         render : function() {
-            this.$el.css('min-width', '16px');
+            this.$el.css({
+                    'width': '16px',
+                    'padding': '4px',
+                });
             this.$support = $('<div />').css({
                     'position': 'relative',
                     'margin': '1px',
                     'width': '4px',
                     'height': '64px',
                     'border': '1px solid black',
-                    'border-radius': '2px',
-                    'background-color': 'lightgray',
-                    'overflow': 'visible',
+                    'background': 'lightgray',
                 })
                 .appendTo(this.$el);
             this.$bullet = $('<div />')
                 .css({
                     'position': 'absolute',
-                    'margin-top': '-5px',
-                    'margin-left': '-4px',
+                    'margin': '-4px',
                     'width': '10px',
                     'height': '10px',
-                    'border-radius': '5px',
-                    'background-color': 'gray',
+                    'background': 'gray',
                 })
                 .appendTo(this.$support);
+            this.$label = $('<div />')
+                .text(this.model.index)
+                .css('text-align', 'center')
+                .appendTo(this.$el);
             this.update();
         },
 
@@ -86,9 +92,10 @@ define([
              * Once one is connected, it will start the update loop, which
              * populates the update of axes and button values.
              */
-            var gamepads = navigator.getGamepads();
-            var pad = gamepads[this.get('index')];
+            var index = this.get('index');
+            var pad = navigator.getGamepads()[index];
             if (pad) {
+                this.index = index;
                 // Set up the main gamepad attributes
                 this.set({
                     name: pad.id,
@@ -99,11 +106,11 @@ define([
                 // Create buttons and axes. When done, start the update loop
                 var that = this;
                 utils.resolve_promises_dict({
-                    buttons: Promise.all(pad.buttons.map(function(button) {
-                        return that._create_button_model();
+                    buttons: Promise.all(pad.buttons.map(function(btn, index) {
+                        return that._create_button_model(index);
                     })),
-                    axes: Promise.all(pad.axes.map(function(button) {
-                        return that._create_axis_model();
+                    axes: Promise.all(pad.axes.map(function(axis, index) {
+                        return that._create_axis_model(index);
                     })),
                 }).then(function(controls) {
                     that.set(controls);
@@ -118,9 +125,9 @@ define([
             /* Populates axes and button values, until the gamepad is disconnected.
              * When the gamepad is disconnection this.reset_gamepad is called.
              */ 
-            var gamepads = navigator.getGamepads();
-            var pad = gamepads[this.get('index')];
-            if (pad) {
+            var index = this.get('index');
+            var pad = navigator.getGamepads()[index];
+            if (pad && this.index === index) {
                 this.set({
                     timestamp: pad.timestamp,
                     connected: pad.connected,
@@ -164,7 +171,7 @@ define([
             window.requestAnimationFrame(_.bind(this.wait_loop, this));
         },
 
-        _create_button_model: function() {
+        _create_button_model: function(index) {
             /* Creates a gamepad button widget.
              */
             return this.widget_manager.create_model({
@@ -172,12 +179,13 @@ define([
                  widget_class: 'gamepad.gamepad.Button',
             }).then(function(model) {
                  return model.request_state().then(function() {
-                     return model;                  
+                     model.index = index;
+                     return model;
                  });
             });
         },
 
-        _create_axis_model: function() {
+        _create_axis_model: function(index) {
             /* Creates a gamepad axis widget.
              */
             return this.widget_manager.create_model({
@@ -185,7 +193,8 @@ define([
                  widget_class: 'gamepad.gamepad.Axis',
             }).then(function(model) {
                  return model.request_state().then(function() {
-                     return model;                  
+                     model.index = index;
+                     return model;
                  });
             });
         },
@@ -211,15 +220,14 @@ define([
                 this.axis_views.update(value);
             }, this);
 
-            this.listenTo(this.model, 'change:id', this.update_label, this);
+            this.listenTo(this.model, 'change:name', this.update_label, this);
         },
 
         render: function(){
             this.$box = this.$el;
-            this.$box.addClass('widget-box');
+            this.$box.addClass('widget-box vbox');
              
             this.$label = $('<div />')
-                .css('height', '20px')
                 .appendTo(this.$box);
 
             this.$axis_box = $('<div />')
@@ -271,7 +279,7 @@ define([
             GamepadView.__super__.remove.apply(this, arguments);
             this.button_views.remove();
             this.axis_views.remove();
-        }
+        },
 
     });
 
