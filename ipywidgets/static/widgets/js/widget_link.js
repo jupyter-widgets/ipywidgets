db6adc0729a4d4792588a740dcb6eb46f03acea6
Sylvain Corlay <sylvain.corlay@gmail.com>
2015-06-26 11:29:54 -0400

    Cleanup on destroy of other models

--- ipywidgets/static/widgets/js/widget_link.js
+++ ipywidgets/static/widgets/js/widget_link.js
@@ -20,33 +20,38 @@ define([
             } finally {
                 this.updating = false;
             }
-        }
+        },
+
+        cleanup: function() {
+            // Unregister from change and destroy events of source and target.
+            if (this.source) {
+                this.source[0].off("change:" + this.source[1], null, this);
+                this.source[0].off("destroy", null, this);
+            }
+            if (this.target) {
+                this.target[0].off("change:" + this.target[1], null, this);
+                this.target[0].off("destroy", null, this);
+            }
+        },
+
     }, {
+
         serializers: _.extend({
             target: {deserialize: widget.unpack_models},
             source: {deserialize: widget.unpack_models},
         }, widget.WidgetModel.serializers),
+
     });
 
     var LinkModel = BaseLinkModel.extend({
+
         initialize: function() {
             this.on("change", this.update_bindings, this);
-            this.once("destroy", function() {
-                if (this.source){
-                    this.source[0].off("change:" + this.source[1], null, this);
-                }
-                if (this.target){
-                    this.target[0].off("change:" + this.target[1], null, this);
-                }
-            }, this);
+            this.once("destroy", this.cleanup, this);
         },
+
         update_bindings: function() {
-            if (this.source) {
-                this.source[0].off("change:" + this.source[1], null, this);
-            }
-            if (this.target) {
-                this.target[0].off("change:" + this.target[1], null, this);
-            }
+            this.cleanup();
             this.source = this.get("source");
             this.target = this.get("target");
             if (this.source) {
@@ -54,28 +59,27 @@ define([
                     this.update_value(this.source, this.target);
                 }, this);
                 this.update_value(this.source, this.target);
+                this.source[0].once("destroy", this.cleanup, this);
             }
             if (this.target) {
                 this.target[0].on("change:" + this.target[1], function() {
                     this.update_value(this.target, this.source);
                 }, this);
+                this.target[0].once("destroy", this.cleanup, this);
             }
         },
+
     });
 
     var DirectionalLinkModel = BaseLinkModel.extend({
+
         initialize: function() {
             this.on("change", this.update_bindings, this);
-            this.once("destroy", function() {
-                if (this.source) {
-                    this.source[0].off("change:" + this.source[1], null, this);
-                }
-            }, this);
+            this.once("destroy", this.cleanup, this);
         },
+
         update_bindings: function() {
-            if (this.source) {
-                this.source[0].off("change:" + this.source[1], null, this);
-            }
+            this.cleanup();
             this.source = this.get("source");
             this.target = this.get("target");
             if (this.source) {
@@ -83,8 +87,13 @@ define([
                     this.update_value(this.source, this.target);
                 }, this);
                 this.update_value(this.source, this.target);
+                this.source[0].once("destroy", this.cleanup, this);
+            }
+            if (this.target) {
+                this.target[0].once("destroy", this.cleanup, this);
             }
         },
+
     });
 
     return {
