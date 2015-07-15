cfa5e3876ef7c52baff45fd139cbbfa9808f368b
Jonathan Frederic <jon.freder@gmail.com>
2015-07-15 08:15:16 -0700

    Call on import

--- ipywidgets/__init__.py
+++ ipywidgets/__init__.py
@@ -1,8 +1,13 @@
 from .widgets import *
+from IPython import get_ipython
 
 # Register a comm target for Javascript initialized widgets..
 def handle_kernel(kernel):
     kernel.comm_manager.register_target('ipython.widget', Widget.handle_comm_opened)
+try:
+	handle_kernel(get_ipython().kernel)
+except:
+	pass # Nom nom nom
 
 # Return the static assets path.
 def find_static_assets():
