969465ff409b16d9654722c419b70547d00819f4
Sylvain Corlay <sylvain.corlay@gmail.com>
2015-11-25 15:29:16 -0500

    Add codemirror instance in the example with Python code generating the example

--- examples/development/web2/index.js
+++ examples/development/web2/index.js
@@ -1,11 +1,24 @@
-// Create a widget manager instance.
+var CodeMirror = require("codemirror");
+require("./node_modules/codemirror/lib/codemirror.css");
+require("./node_modules/codemirror/mode/python/python");
+
 var WidgetManager = require("./manager").WidgetManager;
 
 document.addEventListener("DOMContentLoaded", function(event) {
 
-    var manager = new WidgetManager(document.body);
+    var code = require("./widget_code.json").join("\n");
+    var inputarea = document.getElementsByClassName("inputarea")[0];
+    var editor = CodeMirror(inputarea, {
+        value: code,
+        mode: "python",
+        tabSize: 4,
+        showCursorWhenSelecting: true,
+        viewportMargin: Infinity,
+        readOnly: true
+    });
 
     var state = require("./widget_state.json");
-
+    var widgetarea = document.getElementsByClassName("widgetarea")[0];
+    var manager = new WidgetManager(widgetarea);
     manager.set_state(state);
 });
