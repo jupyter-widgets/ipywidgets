7820b13eb5eddc5cc6b1fb7c0326c3e27d33f213
Jonathan Frederic <jon.freder@gmail.com>
2015-12-07 09:41:10 -0800

    Base karma setup

---
+++ ipywidgets/test/test.js
@@ -0,0 +1,5 @@
+describe("A test suite", function() {
+   beforeEach(function() { });
+   afterEach(function() { });
+   it('should fail', function() { expect(false).to.be.false; });
+});
--- ipywidgets/tests/typings/tsd.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="casperjs/casperjs.d.ts" />
-/// <reference path="es6-promise/es6-promise.d.ts" />
-/// <reference path="phantomjs/phantomjs.d.ts" />
