71bcdb95dfc57b5ffcebd2f6b2d9b5acdeee4520
Min RK <benjaminrk@gmail.com>
2015-12-22 13:21:55 +0100

    add top-level ipywidgets module docstring

--- ipywidgets/__init__.py
+++ ipywidgets/__init__.py
@@ -1,3 +1,21 @@
+"""IPython widgets for Jupyter notebooks.
+
+IPython Widgets provide simple interactive controls in the notebook.
+Widgets provide a mechanism for creating intera
+Each Widget corresponds to an object in Python and Javascript,
+with controls on the page.
+
+To put a Widget on the page, you can display it with IPython's display machinery::
+
+    from ipywidgets import IntSlider
+    from IPython.display import display
+    slider = IntSlider(min=1, max=10)
+    display(slider)
+
+Moving the slider will change the value. Most Widgets have a current value,
+accessible as a `value` attribute.
+"""
+
 import os
 
 from IPython import get_ipython
