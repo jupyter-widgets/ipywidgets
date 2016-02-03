6be35b4c6ede7aa20540326db9fca41b0e7e8ab5
Jonathan Frederic <jon.freder@gmail.com>
2016-02-02 20:40:02 -0800

    Trick the webpack static analysis

--- widgetsnbextension/src/extension.js
+++ widgetsnbextension/src/extension.js
@@ -1,7 +1,7 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
 
-requirejs.config({
+window['requirejs'].config({
     map: {
         "*": {
             "jupyter-js-widgets": "nbextensions/widgets/extension",
