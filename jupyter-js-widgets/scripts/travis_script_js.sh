07e616853f90562eb2c706ef57ac4748fc024aa4
Jonathan Frederic <jon.freder@gmail.com>
2016-02-09 11:04:55 -0800

    Update test scripts so they distinguish between ff/ch for example tests

--- jupyter-js-widgets/scripts/travis_script_js.sh
+++ jupyter-js-widgets/scripts/travis_script_js.sh
@@ -2,4 +2,5 @@ export CHROME_BIN=chromium-browser
 export DISPLAY=:99.0
 sh -e /etc/init.d/xvfb start
 cd jupyter-js-widgets
-npm run test:$BROWSER
+npm run test:unit:$BROWSER
+npm run test:examples:$BROWSER
