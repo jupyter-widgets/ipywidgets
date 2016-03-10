e262460f162456ea762fab94044893efbbb0f828
Jonathan Frederic <jon.freder@gmail.com>
2016-03-10 14:48:59 -0800

    Fix Travis script

--- jupyter-js-widgets/scripts/travis_script_js.sh
+++ jupyter-js-widgets/scripts/travis_script_js.sh
@@ -2,5 +2,4 @@ export CHROME_BIN=chromium-browser
 export DISPLAY=:99.0
 sh -e /etc/init.d/xvfb start
 cd jupyter-js-widgets
-npm run test:unit:$BROWSER
-npm run test:examples:$BROWSER
+npm run test:unit:$BROWSER && npm run test:examples:$BROWSER
