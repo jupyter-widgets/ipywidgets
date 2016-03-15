96c7d0456c7f0c97ed6a9ff47ba4d824fd4f7fb7
Sylvain Corlay <sylvain.corlay@gmail.com>
2016-03-15 19:35:40 -0400

    Add css class to generated widget area

--- jupyter-js-widgets/examples/web4/index.js
+++ jupyter-js-widgets/examples/web4/index.js
@@ -1,10 +1,11 @@
-require("jupyter-js-widgets/css/widgets.min.css");
+require('jupyter-js-widgets/css/widgets.min.css');
 
 var WidgetManager = require("./manager").WidgetManager;
 
 window.w = function renderWidgetState (state) {
     console.info('Inserting widget(s)...');
     var widgetarea = document.createElement('div');
+    widgetarea.className = 'jupyter-widgetarea'
     var manager = new WidgetManager(widgetarea);
     manager.set_state(state);
     var context = Array.prototype.slice.call(document.querySelectorAll('script'), -1)[0];
