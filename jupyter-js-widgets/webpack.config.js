b931bdd679b648f89ef039cefe73f50485c00136
Sylvain Corlay <sylvain.corlay@gmail.com>
2016-04-06 00:47:24 -0400

    Dynamically load version

--- jupyter-js-widgets/webpack.config.js
+++ jupyter-js-widgets/webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
     module: {
         loaders: [
             { test: /\.css$/, loader: "style-loader!css-loader" },
+            { test: /\.json$/, loader: "json-loader" },
             // jquery-ui loads some images
             { test: /\.(jpg|png|gif)$/, loader: "file" },
             // required to load font-awesome
