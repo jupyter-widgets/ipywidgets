e5c3d972868755dad3c15771ffed896eddede614
Sylvain Corlay <sylvain.corlay@gmail.com>
2016-04-09 22:44:06 -0400

    Fix publicPath in web examples

--- jupyter-js-widgets/examples/web2/webpack.config.js
+++ jupyter-js-widgets/examples/web2/webpack.config.js
@@ -2,7 +2,8 @@ module.exports = {
     entry: './index.js',
     output: {
         filename: 'index.built.js',
-        path: './built/'
+        path: './built/',
+        publicPath: 'built/'
     },
     module: {
         loaders: [
