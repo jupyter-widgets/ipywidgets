efacaedd8f856ec3496b70f234eedbe2b79dd67f
Jonathan Frederic <jon.freder@gmail.com>
2016-05-18 09:28:25 -0700

    Check for npm and pip

--- dev-install.sh
+++ dev-install.sh
@@ -1,6 +1,25 @@
 #!/usr/bin/env bash
 nbExtFlags=$1
 
+npm -v
+if [ $? -eq 0 ]; then
+    echo npm is installed
+else
+    echo "'npm -v' failed therefore npm is not installed.  In
+    order to perform a developer install of ipywidgets you must
+    have both npm and pip installed on your machine!"
+    exit 1
+fi
+pip --version
+if [ $? -eq 0 ]; then
+    echo pip is installed
+else
+    echo "'pip --version' failed. therefore pip is not installed.
+    In order to perform a developer install of ipywidgets you
+    must have both pip and npm installed on your machine!"
+    exit 1
+fi
+
 cd jupyter-js-widgets
 npm install
 cd ..
