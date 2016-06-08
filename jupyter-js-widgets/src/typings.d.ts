0d2415e0b69ecaebc69280d4830c18b313e8b765
Jason Grout <jgrout6@bloomberg.net>
2016-06-08 18:27:34 -0400

    Enable typescript files in the source directory.

---
+++ jupyter-js-widgets/src/typings.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="../typings/es6-promise/es6-promise.d.ts"/>
+/// <reference path="../typings/backbone/backbone.d.ts"/>
+/// <reference path="../typings/mathjax/mathjax.d.ts"/>
+/// <reference path="../typings/require/require.d.ts"/>
