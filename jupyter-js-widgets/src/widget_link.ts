a6ea6373dce2062a71c141bac1145199f43ea062
Jason Grout <jgrout6@bloomberg.net>
2016-06-15 14:41:06 -0400

    Refactor link widget.

--- jupyter-js-widgets/src/widget_link.ts
+++ jupyter-js-widgets/src/widget_link.ts
@@ -1,114 +1,104 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
-'use strict';
 
-var widget = require('./widget');
-var _ = require('underscore');
+import {
+    DOMWidgetModel, DOMWidgetView, WidgetModel, unpack_models
+} from './widget';
 
-var BaseLinkModel = widget.WidgetModel.extend({
+import * as _ from 'underscore';
 
-    defaults: _.extend({}, widget.WidgetModel.prototype.defaults, {
-        target: undefined,
-        source: undefined
-    }),
 
-    update_value: function(source, target) {
-        if (this.updating) {
+export
+class DirectionalLinkModel extends WidgetModel {
+    static serializers = _.extend({
+        target: {deserialize: unpack_models},
+        source: {deserialize: unpack_models}
+    }, WidgetModel.serializers)
+
+    defaults() {
+        return _.extend(super.defaults(), {
+            target: undefined,
+            source: undefined,
+            _model_name: 'DirectionalLinkModel'
+        });
+    }
+
+    initialize() {
+        this.on('change', this.updateBindings, this);
+        this.updateBindings();
+    }
+
+    updateValue(sourceModel, sourceAttr, targetModel, targetAttr) {
+        if (this._updating) {
             return;
         }
-        this.updating = true;
+        this._updating = true;
         try {
-            if (target[0]) {
-                target[0].set(target[1], source[0].get(source[1]));
-                target[0].save_changes();
+            if (targetModel) {
+                targetModel.set(targetAttr, sourceModel.get(sourceAttr));
+                targetModel.save_changes();
             }
         } finally {
-            this.updating = false;
+            this._updating = false;
         }
-    },
+    }
 
-    cleanup: function() {
+    updateBindings() {
+        this.cleanup();
+        [this.sourceModel, this.sourceAttr] = this.get('source');
+        [this.targetModel, this.targetAttr] = this.get('target');
+        if (this.sourceModel) {
+            this.listenTo(this.sourceModel, 'change:' + this.sourceAttr, () => {
+                this.updateValue(this.sourceModel, this.sourceAttr, this.targetModel, this.targetAttr);
+            });
+            this.updateValue(this.sourceModel, this.sourceAttr, this.targetModel, this.targetAttr);
+            this.listenToOnce(this.sourceModel, 'destroy', this.cleanup);
+        }
+        if (this.targetModel) {
+            this.listenToOnce(this.targetModel, 'destroy', this.cleanup);
+        }
+    }
+
+    cleanup() {
         // Stop listening to 'change' and 'destroy' events of the source and target
-        if (this.source) {
-            this.stopListening(this.source[0], 'change:' + this.source[1], null, this);
-            this.stopListening(this.source[0], 'destroy', null, this);
+        if (this.sourceModel) {
+            this.stopListening(this.sourceModel, 'change:' + this.sourceAttr, null);
+            this.stopListening(this.sourceModel, 'destroy', null);
         }
-        if (this.target) {
-            this.stopListening(this.target[0], 'change:' + this.target[1], null, this);
-            this.stopListening(this.target[0], 'destroy', null, this);
-        }
-    }
-
-}, {
-
-    serializers: _.extend({
-        target: {deserialize: widget.unpack_models},
-        source: {deserialize: widget.unpack_models}
-    }, widget.WidgetModel.serializers)
-
-});
-
-var LinkModel = BaseLinkModel.extend({
-
-    defaults: _.extend({}, BaseLinkModel.prototype.defaults, {
-        _model_name: 'LinkModel'
-    }),
-
-    initialize: function() {
-        this.on('change', this.update_bindings, this);
-        this.update_bindings();
-    },
-
-    update_bindings: function() {
-        this.cleanup();
-        this.source = this.get('source');
-        this.target = this.get('target');
-        if (this.source) {
-            this.listenTo(this.source[0], 'change:' + this.source[1], function() {
-                this.update_value(this.source, this.target);
-            }, this);
-            this.update_value(this.source, this.target);
-            this.listenToOnce(this.source[0], 'destroy', this.cleanup, this);
-        }
-        if (this.target) {
-            this.listenTo(this.target[0], 'change:' + this.target[1], function() {
-                this.update_value(this.target, this.source);
-            }, this);
-            this.listenToOnce(this.target[0], 'destroy', this.cleanup, this);
-        }
-    }
-});
-
-var DirectionalLinkModel = BaseLinkModel.extend({
-
-    defaults: _.extend({}, BaseLinkModel.prototype.defaults, {
-        _model_name: 'DirectionalLinkModel'
-    }),
-
-    initialize: function() {
-        this.on('change', this.update_bindings, this);
-        this.update_bindings();
-    },
-
-    update_bindings: function() {
-        this.cleanup();
-        this.source = this.get('source');
-        this.target = this.get('target');
-        if (this.source) {
-            this.listenTo(this.source[0], 'change:' + this.source[1], function() {
-                this.update_value(this.source, this.target);
-            }, this);
-            this.update_value(this.source, this.target);
-            this.listenToOnce(this.source[0], 'destroy', this.cleanup, this);
-        }
-        if (this.target) {
-            this.listenToOnce(this.target[0], 'destroy', this.cleanup, this);
+        if (this.targetModel) {
+            this.stopListening(this.targetModel, 'destroy', null);
         }
     }
 
-});
+    sourceModel: WidgetModel;
+    sourceAttr: string;
+    targetModel: WidgetModel;
+    targetAttr: string;
 
-module.exports = {
-    LinkModel: LinkModel,
-    DirectionalLinkModel: DirectionalLinkModel
-};
+    private _updating: boolean;    
+}
+
+export
+class LinkModel extends DirectionalLinkModel {
+    defaults() {
+        return _.extend(super.defaults(), {
+            _model_name: 'LinkModel'
+        });
+    }
+
+    updateBindings() {
+        super.updateBindings();
+        if (this.targetModel) {
+            this.listenTo(this.targetModel, 'change:' + this.targetAttr, () => {
+                this.updateValue(this.targetModel, this.targetAttr, this.sourceModel, this.sourceAttr);
+            });
+        }
+    }
+
+    cleanup() {
+        super.cleanup();
+        if (this.targetModel) {
+            this.stopListening(this.targetModel, 'change:' + this.targetAttr, null);
+        }
+    }
+}
