90d503a661142c9fc57dcfd70dac06d9d7907030
Sylvain Corlay <sylvain.corlay@gmail.com>
2016-07-14 15:05:38 -0500

    Release 5.2.1

--- ipywidgets/_version.py
+++ ipywidgets/_version.py
@@ -1,3 +1,3 @@
-version_info = (5, 2, 0)
+version_info = (5, 2, 1)
 __version__ = '.'.join(map(str, version_info))
 __frontend_version__ = '~1.2.0'
--- widgetsnbextension/widgetsnbextension/_version.py
+++ widgetsnbextension/widgetsnbextension/_version.py
@@ -1,2 +1,2 @@
-version_info = (1, 2, 4)
+version_info = (1, 2, 5)
 __version__ = '.'.join(map(str, version_info))
