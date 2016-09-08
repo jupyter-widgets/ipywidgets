74d6bc4f5fe420ad4ad0c91d1475ad3b882774fb
Jeroen Demeyer <jdemeyer@cage.ugent.be>
2016-09-08 15:35:10 +0200

    Add a get_interact_value() method to customize the value passed to interactive functions

--- ipywidgets/widgets/valuewidget.py
+++ ipywidgets/widgets/valuewidget.py
@@ -8,3 +8,10 @@ from .widget import Widget
 
 class ValueWidget(Widget):
     """Widget that can be used for the input of an interactive function"""
+
+    def get_interact_value(self):
+        """Return the value for this widget which should be passed to
+        interactive functions. Custom widgets can change this method
+        to process the raw value ``self.value``.
+        """
+        return self.value
