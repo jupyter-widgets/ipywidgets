64cc50cca11abff0e2ace18e2de296dd5c95efb7
Jason Grout <jgrout6@bloomberg.net>
2016-10-07 22:17:03 -0400

    Clean up jupyterlab_widgets

--- jupyterlab_widgets/externals.js
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Jupyter Development Team.
-// Distributed under the terms of the Modified BSD License.
-
-var helpers = require('jupyterlab/scripts/extension_helpers');
-console.log('providing jupyter-js-widgets shim handler');
-module.exports = helpers.createShimHandler('jupyter-js-widgets');
