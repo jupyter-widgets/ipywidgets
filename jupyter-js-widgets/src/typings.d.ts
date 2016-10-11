6a004de1742b551e9f081a8a38649d06925d5c22
Jason Grout <jgrout6@bloomberg.net>
2016-10-11 13:18:25 -0400

    Update jupyter-js-widgets’ dependency on jupyter-js-services.

--- jupyter-js-widgets/src/typings.d.ts
+++ jupyter-js-widgets/src/typings.d.ts
@@ -1,4 +1,10 @@
-/// <reference path="../typings/es6-promise/es6-promise.d.ts"/>
-/// <reference path="../typings/backbone/backbone.d.ts"/>
-/// <reference path="../typings/mathjax/mathjax.d.ts"/>
-/// <reference path="../typings/semver/semver.d.ts"/>
+/*
+ * TODO: remove the below typings after typedoc understands the lib compiler option
+ * and the @types typing resolution.
+ * When this happens, use the typescript compiler option:
+ * "lib": ["dom", "es5", "es2015.promise"],
+ */
+/// <reference path="../node_modules/typescript/lib/lib.es2015.promise.d.ts"/>
+/// <reference path="../node_modules/typescript/lib/lib.dom.d.ts"/>
+/// <reference path="../node_modules/typescript/lib/lib.es5.d.ts"/>
+/// <reference path="../node_modules/@types/mathjax/index.d.ts"/>
--- jupyter-js-widgets/test/src/typings.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="../../typings/expect.js/expect.js.d.ts"/>
-/// <reference path="../../typings/mocha/mocha.d.ts"/>
-/// <reference path="../../typings/es6-promise/es6-promise.d.ts"/>
-/// <reference path="../../typings/backbone/backbone.d.ts"/>
-/// <reference path="../../typings/sinon/sinon.d.ts"/>
