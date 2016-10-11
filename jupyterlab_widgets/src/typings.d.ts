670f612ecbbbe03c6cb5ad23f1aaa515417be389
Jason Grout <jgrout6@bloomberg.net>
2016-10-11 13:41:48 -0400

    Update to jupyter-js-widgets 2.0.2, which cleans up typings.

--- jupyterlab_widgets/src/typings.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="../typings/backbone/backbone.d.ts"/>
-/// <reference path="../typings/codemirror/codemirror.d.ts"/>
-/// <reference path="../typings/es6-promise/es6-promise.d.ts"/>
-/// <reference path="../node_modules/@types/node/index.d.ts"/>
-/// <reference path="../typings/semver/semver.d.ts"/>
