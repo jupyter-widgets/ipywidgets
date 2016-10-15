41b1deddad4f815cc7413c9a7c5e2d3ccae04ac3
Jason Grout <jgrout6@bloomberg.net>
2016-10-15 04:31:58 -0400

    Clean up typings.

--- jupyter-js-widgets/src/typedoc.d.ts
+++ jupyter-js-widgets/src/typedoc.d.ts
@@ -1,10 +1,9 @@
 /*
  * TODO: remove the below typings after typedoc understands the lib compiler option
  * and the @types typing resolution.
- * When this happens, use the typescript compiler option:
- * "lib": ["dom", "es5", "es2015.promise"],
  */
+/// <reference path="../node_modules/@types/mathjax/index.d.ts"/>
+/// <reference path="../node_modules/@types/requirejs/index.d.ts"/>
 /// <reference path="../node_modules/typescript/lib/lib.es2015.promise.d.ts"/>
 /// <reference path="../node_modules/typescript/lib/lib.dom.d.ts"/>
 /// <reference path="../node_modules/typescript/lib/lib.es5.d.ts"/>
-/// <reference path="../node_modules/@types/mathjax/index.d.ts"/>
