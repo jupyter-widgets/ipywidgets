7e87f988a2c99e19bf132c9f2f613572b1ae4d3f
Jason Grout <jgrout6@bloomberg.net>
2016-11-24 02:09:23 -0500

    Release new versions of ipywidgets, widgetsnbextension, and jupyterlab_widgets.

--- ipywidgets/_version.py
+++ ipywidgets/_version.py
@@ -1,3 +1,3 @@
-version_info = (6, 0, 0, 'beta2')
+version_info = (6, 0, 0, 'beta3')
 __version__ = '.'.join(map(str, version_info))
-__frontend_version__ = '^2.0.4'
+__frontend_version__ = '^2.0.6'
