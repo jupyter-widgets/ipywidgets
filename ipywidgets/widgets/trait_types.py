9c2663cef7e8f0fece8e9874159538260b0798cb
Sylvain Corlay <sylvain.corlay@gmail.com>
2017-01-02 11:38:03 +0100

    Adding date picker widget

--- ipywidgets/widgets/trait_types.py
+++ ipywidgets/widgets/trait_types.py
@@ -8,8 +8,7 @@ Trait types for html widgets.
 
 import re
 import traitlets
-
-from . import eventful
+import datetime as dt
 
 _color_names = ['aliceblue', 'antiquewhite', 'aqua', 'aquamarine', 'azure', 'beige', 'bisque', 'black', 'blanchedalmond', 'blue', 'blueviolet', 'brown', 'burlywood', 'cadetblue', 'chartreuse', 'chocolate', 'coral', 'cornflowerblue', 'cornsilk', 'crimson', 'cyan', 'darkblue', 'darkcyan', 'darkgoldenrod', 'darkgray', 'darkgreen', 'darkkhaki', 'darkmagenta', 'darkolivegreen', 'darkorange', 'darkorchid', 'darkred', 'darksalmon', 'darkseagreen', 'darkslateblue', 'darkslategray', 'darkturquoise', 'darkviolet', 'deeppink', 'deepskyblue', 'dimgray', 'dodgerblue', 'firebrick', 'floralwhite', 'forestgreen', 'fuchsia', 'gainsboro', 'ghostwhite', 'gold', 'goldenrod', 'gray', 'green', 'greenyellow', 'honeydew', 'hotpink', 'indianred ', 'indigo ', 'ivory', 'khaki', 'lavender', 'lavenderblush', 'lawngreen', 'lemonchiffon', 'lightblue', 'lightcoral', 'lightcyan', 'lightgoldenrodyellow', 'lightgray', 'lightgreen', 'lightpink', 'lightsalmon', 'lightseagreen', 'lightskyblue', 'lightslategray', 'lightsteelblue', 'lightyellow', 'lime', 'limegreen', 'linen', 'magenta', 'maroon', 'mediumaquamarine', 'mediumblue', 'mediumorchid', 'mediumpurple', 'mediumseagreen', 'mediumslateblue', 'mediumspringgreen', 'mediumturquoise', 'mediumvioletred', 'midnightblue', 'mintcream', 'mistyrose', 'moccasin', 'navajowhite', 'navy', 'oldlace', 'olive', 'olivedrab', 'orange', 'orangered', 'orchid', 'palegoldenrod', 'palegreen', 'paleturquoise', 'palevioletred', 'papayawhip', 'peachpuff', 'peru', 'pink', 'plum', 'powderblue', 'purple', 'rebeccapurple', 'red', 'rosybrown', 'royalblue', 'saddlebrown', 'salmon', 'sandybrown', 'seagreen', 'seashell', 'sienna', 'silver', 'skyblue', 'slateblue', 'slategray', 'snow', 'springgreen', 'steelblue', 'tan', 'teal', 'thistle', 'tomato', 'turquoise', 'violet', 'wheat', 'white', 'whitesmoke', 'yellow', 'yellowgreen'] 
 _color_re = re.compile(r'#[a-fA-F0-9]{3}(?:[a-fA-F0-9]{3})?$')
@@ -27,45 +26,50 @@ class Color(traitlets.Unicode):
         self.error(obj, value)
 
 
-class EventfulDict(traitlets.Instance):
-    """An instance of an EventfulDict."""
+class Datetime(traitlets.TraitType):
+    """A trait type holding a Python datetime object"""
 
-    def __init__(self, default_value={}, **metadata):
-        """Create a EventfulDict trait type from a dict.
+    klass = dt.datetime
+    default_value = dt.datetime(1900, 1, 1)
 
-        The default value is created by doing
-        ``eventful.EvenfulDict(default_value)``, which creates a copy of the
-        ``default_value``.
-        """
-        if default_value is None:
-            args = None
-        elif isinstance(default_value, dict):
-            args = (default_value,)
-        elif isinstance(default_value, SequenceTypes):
-            args = (default_value,)
-        else:
-            raise TypeError('default value of EventfulDict was %s' % default_value)
+def datetime_to_json(pydt, manager):
+    """Serializes a Python datetime object to json.
 
-        super(EventfulDict, self).__init__(klass=eventful.EventfulDict, args=args,
-                                           **metadata)
+    Instantiating a JavaScript Date object with a string assumes that the
+    string is a UTC string, while instantiating it with constructor arguments
+    assumes that it's in local time:
 
+    >>> cdate = new Date('2015-05-12')
+    Mon May 11 2015 20:00:00 GMT-0400 (Eastern Daylight Time)
+    >>> cdate = new Date(2015, 4, 12) // Months are 0-based indices in JS
+    Tue May 12 2015 00:00:00 GMT-0400 (Eastern Daylight Time)
 
-class EventfulList(traitlets.Instance):
-    """An instance of an EventfulList."""
+    Attributes of this dictionary are to be passed to the JavaScript Date
+    constructor.
+    """
+    return dict(
+        year=pydt.year,
+        month=pydt.month - 1,  # Months are 0-based indices in JS
+        date=pydt.day,
+        hours=pydt.hour,       # Hours, Minutes, Seconds and Milliseconds are
+        minutes=pydt.minute,   # plural in JS
+        seconds=pydt.second,
+        milliseconds=pydt.microsecond / 1000
+    )
 
-    def __init__(self, default_value=None, **metadata):
-        """Create a EventfulList trait type from a dict.
+def datetime_from_json(js, manager):
+    """Deserialize a Python datetime object from json"""
+    return dt.datetime(
+        js['year'],
+        js['month'] + 1,  # Months are 1-based in Python
+        js['date'],
+        js['hours'],
+        js['minutes'],
+        js['seconds'],
+        js['milliseconds'] * 1000
+    )
 
-        The default value is created by doing 
-        ``eventful.EvenfulList(default_value)``, which creates a copy of the 
-        ``default_value``.
-        """
-        if default_value is None:
-            args = ((),)
-        else:
-            args = (default_value,)
-
-        super(EventfulList, self).__init__(klass=eventful.EventfulList, args=args,
-                                           **metadata)
-
-
+datetime_serialization = {
+    'from_json': datetime_from_json,
+    'to_json': datetime_to_json
+}
