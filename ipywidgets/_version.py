e24ecd962bd224f219e3e3c4388534ea46db5299
Jason Grout <jgrout6@bloomberg.net>
2017-01-31 16:28:06 -0500

    ipywidgets 6.0.0b7, jupyter-js-widgets 2.0.25, widgetsnbextension 2.0.0b14

--- ipywidgets/_version.py
+++ ipywidgets/_version.py
@@ -1,3 +1,3 @@
-version_info = (6, 0, 0, 'beta6')
+version_info = (6, 0, 0, 'beta7')
 __version__ = '.'.join(map(str, version_info))
 __frontend_version__ = '^2.0.19'
--- widgetsnbextension/widgetsnbextension/_version.py
+++ widgetsnbextension/widgetsnbextension/_version.py
@@ -1,2 +1,2 @@
-version_info = (2, 0, 0, 'beta12')
+version_info = (2, 0, 0, 'beta14')
 __version__ = '.'.join(map(str, version_info))
