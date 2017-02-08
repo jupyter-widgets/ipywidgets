812d747fe8a375a48446e6d7f1ca75065e158448
Jason Grout <jgrout6@bloomberg.net>
2017-02-08 17:53:58 -0500

    ipywidgets 6.0.0b9, jupyter-js-widgets 2.0.28, jupyterlab_widgets 0.6.10, widgetsnbextension 2.0.0b16

--- ipywidgets/_version.py
+++ ipywidgets/_version.py
@@ -1,3 +1,3 @@
-version_info = (6, 0, 0, 'beta8')
+version_info = (6, 0, 0, 'beta9')
 __version__ = '.'.join(map(str, version_info))
-__frontend_version__ = '~2.0.27'
+__frontend_version__ = '~2.0.28'
