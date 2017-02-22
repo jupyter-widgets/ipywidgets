7cf08cb61b42a40ca9cf02379bf00a158b443981
Jason Grout <jgrout6@bloomberg.net>
2017-02-22 14:11:46 -0500

    6.0.0rc2

--- ipywidgets/_version.py
+++ ipywidgets/_version.py
@@ -1,6 +1,6 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (6, 0, 0, 'rc1')
+version_info = (6, 0, 0, 'rc2')
 __version__ = '.'.join(map(str, version_info))
 __frontend_version__ = '~2.1.0'
