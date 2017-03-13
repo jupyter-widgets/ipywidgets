647c4fde9fbd27b4a075bafd83c12e858486eccd
Jason Grout <jgrout6@bloomberg.net>
2017-03-13 16:58:21 -0400

    Add a BeforeAttach message when attaching widgets to the DOM.

--- widgetsnbextension/src/widgetarea.js
+++ widgetsnbextension/src/widgetarea.js
@@ -38,6 +38,7 @@ WidgetArea.prototype.display_widget_view = function(view_promise) {
 
         // Do basically the same as Phosphor's Widget.attach, except we replace
         // a child rather than appending the view's Phosphor widget
+        messaging.MessageLoop.sendMessage(view.pWidget, pwidgets.Widget.Msg.BeforeAttach)
         dummy.parentNode.replaceChild(view.pWidget.node, dummy);
         messaging.MessageLoop.sendMessage(view.pWidget, pwidgets.Widget.Msg.AfterAttach)
 
