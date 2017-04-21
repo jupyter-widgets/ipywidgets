b68ca4518ab4d1bab4cce8c46d1a51df94dc23cd
Jason Grout <jgrout6@bloomberg.net>
2017-04-21 15:08:57 -0400

    Export v2 schema.

--- jupyter-widgets-schema/index.js
+++ jupyter-widgets-schema/index.js
@@ -2,5 +2,9 @@ module.exports = {
     v1: {
         state: require('./v1/state.schema.json'),
         view: require('./v1/view.schema.json')
-    }
+    },
+    v2: {
+        state: require('./v2/state.schema.json'),
+        view: require('./v2/view.schema.json')
+    },
 };
