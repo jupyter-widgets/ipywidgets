c87ff49db6bda3c16a70113ca60b61a4621df433
Janki Akhani <jakhani@linkedin.com>
2017-04-26 16:43:28 +0530

    Adding type=password support for Text in TextView class.

--- ipywidgets/widgets/tests/test_interaction.py
+++ ipywidgets/widgets/tests/test_interaction.py
@@ -89,17 +89,6 @@ def test_single_value_string():
         value=a,
     )
 
-def test_single_value_password_string():
-    a = u'hello'
-    c = interactive(f, a=a)
-    w = c.children[0]
-    check_widget(w,
-                 cls=widgets.Password,
-                 description='a',
-                 value=a,
-                 )
-
-
 def test_single_value_bool():
     for a in (True, False):
         c = interactive(f, a=a)
