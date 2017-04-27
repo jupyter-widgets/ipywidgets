047b3d3fa365451f4161f0eb002a1a2733d315a7
Jason Grout <jgrout6@bloomberg.net>
2017-04-27 11:35:54 -0400

    ipywidgets 7.0.0a0, widgetsnbextension 3.0.0a1, jupyter-js-widgets 3.0.0-alpha.0, @jupyterlab/nbwidgets 0.6.16

--- widgetsnbextension/widgetsnbextension/_version.py
+++ widgetsnbextension/widgetsnbextension/_version.py
@@ -1,2 +1,6 @@
-version_info = (2, 0, 0)
-__version__ = '.'.join(map(str, version_info))
+version_info = (3, 0, 0, 'alpha', 1)
+
+_specifier_ = {'alpha': 'a', 'beta': 'b', 'candidate': 'rc', 'final': ''}
+
+__version__ = '%s.%s.%s%s'%(version_info[0], version_info[1], version_info[2],
+  '' if version_info[3]=='final' else _specifier_[version_info[3]]+str(version_info[4]))
