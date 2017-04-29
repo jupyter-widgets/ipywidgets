9d021e25ea9c5d1448adde751e3826f66dca54fb
Jason Grout <jgrout6@bloomberg.net>
2017-04-29 08:39:38 -0400

    Break up manager base tests into finer sections.

--- jupyter-js-widgets/test/src/manager_test.ts
+++ jupyter-js-widgets/test/src/manager_test.ts
@@ -16,42 +16,59 @@ describe("ManagerBase", function() {
 
     });
 
-    it('comm_target_name', function() {
-        expect(this.managerBase.comm_target_name).to.equal('jupyter.widget');
+    describe('comm_target_name', function() {
+        it('exists', function() {
+          expect(this.managerBase.comm_target_name).to.equal('jupyter.widget');
+        });
     });
 
-    it('display_model', function() {
-      expect(this.managerBase.display_model).to.not.be.undefined;
+    describe('display_model', function() {
+      it('exists', function() {
+        expect(this.managerBase.display_model).to.not.be.undefined;
+      });
     });
 
-    it('setViewOptions', function() {
-      expect(this.managerBase.setViewOptions).to.not.be.undefined;
+    describe('setViewOptions', function() {
+      it('exists', function() {
+        expect(this.managerBase.setViewOptions).to.not.be.undefined;
+      });
     });
 
-    it('create_view', function() {
-      expect(this.managerBase.create_view).to.not.be.undefined;
+    describe('create_view', function() {
+      it('exists', function() {
+        expect(this.managerBase.create_view).to.not.be.undefined;
+      });
     });
 
-    it('callbacks', function() {
-      // Cell-less call
-      let c = this.managerBase.callbacks();
-      expect(c).to.be.an('object');
-      expect(c.iopub).to.be.undefined;
+    describe('callbacks', function() {
+      it('returns an object without an iopub', function() {
+        // Cell-less call
+        let c = this.managerBase.callbacks();
+        expect(c).to.be.an('object');
+        expect(c.iopub).to.be.undefined;
+      });
     });
 
-    it('get_model', function() {
-      expect(this.managerBase.get_model).to.not.be.undefined;
+    describe('get_model', function() {
+      it('exists', function() {
+        expect(this.managerBase.get_model).to.not.be.undefined;
+      });
     });
 
-    it('handle_comm_open', function() {
-      expect(this.managerBase.handle_comm_open).to.not.be.undefined;
+    describe('handle_comm_open', function() {
+      it('exists', function() {
+        expect(this.managerBase.handle_comm_open).to.not.be.undefined;
+      });
     });
 
-    it('new_widget', function() {
-      expect(this.managerBase.new_widget).to.not.be.undefined;
+    describe('new_widget', function() {
+      it('exists', function() {
+        expect(this.managerBase.new_widget).to.not.be.undefined;
+      });
     });
 
-    it('new_model', async function() {
+    describe('new_model', function() {
+      it('model has a get', async function() {
       let model = await this.managerBase.new_model({
             model_name: 'IntSlider',
             model_module: 'jupyter-js-widgets',
@@ -59,20 +76,35 @@ describe("ManagerBase", function() {
             model_id: 'u-u-i-d'
         });
       expect(model).to.have.property('get');
+    });
+    it('model is stored in manager _models', async function() {
+      let model = await this.managerBase.new_model({
+            model_name: 'IntSlider',
+            model_module: 'jupyter-js-widgets',
+            model_module_version: '3.0.0',
+            model_id: 'u-u-i-d'
+        });
       let storedModel = await this.managerBase._models[model.id];
       expect(storedModel).to.equal(model);
     });
 
-    it('clear_state', function() {
-      expect(this.managerBase.clear_state).to.not.be.undefined;
+  });
+    describe('clear_state', function() {
+      it('exists', function() {
+        expect(this.managerBase.clear_state).to.not.be.undefined;
+      });
     });
 
-    it('get_state', function() {
-      expect(this.managerBase.get_state).to.not.be.undefined;
+    describe('get_state', function() {
+      it('exists', function() {
+        expect(this.managerBase.get_state).to.not.be.undefined;
+      });
     });
 
-    it('set_state', function() {
-      expect(this.managerBase.set_state).to.not.be.undefined;
+    describe('set_state', function() {
+      it('exists', function() {
+        expect(this.managerBase.set_state).to.not.be.undefined;
+      });
     });
 });
 
