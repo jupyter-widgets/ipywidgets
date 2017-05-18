58886d5a36d7c60c2be3b4fe505bb9cdb6dd4a90
Jason Grout <jgrout6@bloomberg.net>
2017-05-18 15:51:41 -0400

    Build docs in travis

--- scripts/travis_script_py.sh
+++ scripts/travis_script_py.sh
@@ -1,3 +1,9 @@
 mkdir /tmp/ipywidgets
 cd /tmp/ipywidgets
 nosetests --with-coverage --cover-package=ipywidgets ipywidgets
+
+# Verify docs build
+pushd docs
+pip install -r requirements.txt
+make html
+popd
