edb6ce87b2e66c03965799f14eb56c9bc079fc4c
Pascal Bugnion <pascal@bugnion.org>
2017-05-28 11:49:38 +0100

    Initial commit for new embed-jupyter-widgets package

---
+++ embed-jupyter-widgets/webpack.config.js
@@ -0,0 +1,49 @@
+// Copyright (c) Jupyter Development Team.
+// Distributed under the terms of the Modified BSD License.
+
+var version = require('./package.json').version;
+
+module.exports = {
+    entry: './lib/embed-webpack.js',
+    output: {
+        filename : 'embed.js',
+        path: './dist',
+        publicPath: 'https://unpkg.com/embed-jupyter-widgets@' + version + '/dist/'
+    },
+    devtool: 'source-map',
+    module: {
+        loaders: [
+            { test: /\.css$/, loader: "style-loader!css-loader!postcss-loader" },
+            // { test: /\.css$/,
+            //   loaders: [
+            //       "style-loader",
+            //       "css-loader",
+            //       {
+            //           loader: "postcss-loader",
+            //           options: {
+            //               plugins: (loader) => [
+            //                   require('postcss-import')({ root: loader.resourcePath }),
+            //                   require('postcss-cssnext')()
+            //               ]
+            //           }
+            //       }
+            //   ]
+            // },
+            { test: /\.json$/, loader: "json-loader" },
+            // jquery-ui loads some images
+            { test: /\.(jpg|png|gif)$/, loader: "file" },
+            // required to load font-awesome
+            { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
+            { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
+            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
+            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
+            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" }
+        ]
+    },
+    postcss: function () {
+        return [
+            require('postcss-import'),
+            require('postcss-cssnext')
+        ];
+    }
+};
