492ccf414f36f7f97fabc84823950c9609544639
Vidar Tonaas Fauske <vidartf@gmail.com>
2017-05-29 16:55:07 +0200

    Simplify embed resolution
    Normally, the only referring widgets you would want to add are (fully
    resolved) links, so this commit removes the resolution of referring
    widgets, and instead adds a tailored function to include any such fully
    resolved links.

--- ipywidgets/widgets/embed.py
+++ ipywidgets/widgets/embed.py
@@ -33,6 +33,7 @@ Functions for generating embeddable HTML/javascript of a widget.
 import json
 from .widget import Widget, _remove_buffers
 from .domwidget import DOMWidget
+from .widget_link import Link
 
 
 snippet_template = """<script src="{embed_url}"></script>
@@ -95,31 +96,19 @@ def get_recursive_state(widget, store=None, drop_defaults=False):
     return store
 
 
-def add_referring_widgets(store, drop_defaults):
-    """Add state of any widgets referring to widgets already in the store"""
-    found_new = False
+def add_resolved_links(store, drop_defaults):
+    """Checks if any link models exists between models in store"""
     for widget_id, widget in Widget.widgets.items(): # go over all widgets
-        if widget_id not in store:
-            widget_state = widget.get_state(drop_defaults=drop_defaults)
-            widget_state = _remove_buffers(widget_state)[0]
-            # Loop over all references in current widget state:
-            for ref in _find_widget_refs_by_state(widget, widget_state):
-                # If the found ref is already in state, include the found reference
-                if ref.model_id in store:
-                    store[widget.model_id] = widget._get_embed_state(drop_defaults=drop_defaults)
-                    found_new = True
-    return found_new
+        if isinstance(widget, Link) and widget_id not in store:
+            if widget.source[0].model_id in store and widget.target[0].model_id in store:
+                store[widget.model_id] = widget._get_embed_state(drop_defaults=drop_defaults)
 
 
-def dependency_state(widgets, drop_defaults, dependents=True):
+def dependency_state(widgets, drop_defaults):
     """Get the state of all widgets specified, and their dependencies.
 
-    If `dependents` is True (the default), widgets which depend on any of the
-    resolved widgets will be added as well.
-
-    In the below graph, D and E are depencies of C; A and B are dependents of C;
-    and F is an dependent of E. That means the state will include (C, D, E) for
-    dependents=False, and (A, B, C, D, E, F) for dependents=True.
+    In the below graph, D and E are depencies of C; C is a dependency of both A and B;
+    and E is a dependency of F. That means the state of C will include (C, D, E).
 
     A --           -- D
         | -- C -- |
@@ -128,24 +117,26 @@ def dependency_state(widgets, drop_defaults, dependents=True):
                  F --
 
     ---- Dependecy ---->
+
+    Note: Any links between included widgets will also be added. Using the example
+    above, this means that any links between widgets (C, D, E) will also be included.
+
     """
     # collect the state of all relevant widgets
     if widgets is None:
+        # Get state of all widgets, no smart resolution needed.
         widgets = Widget.widgets.values()
         state = Widget.get_manager_state(drop_defaults=drop_defaults, widgets=widgets)['state']
     else:
         state = {}
         for widget in widgets:
             get_recursive_state(widget, state, drop_defaults)
-        if dependents:
-            # it may be that other widgets refer to the collected widgets,
-            # such as layouts, include those as well
-            while add_referring_widgets(state, drop_defaults):
-                pass
+        # Add any links between included widgets:
+        add_resolved_links(state, drop_defaults)
     return state
 
 
-def embed_data(widgets, expand_dependencies='full', drop_defaults=True):
+def embed_data(widgets, include_dependencies=True, drop_defaults=True):
     """Gets data for embedding.
 
     Use this to get the raw data for embedding if you have special
@@ -160,9 +151,8 @@ def embed_data(widgets, expand_dependencies='full', drop_defaults=True):
             widgets[0]
         except (IndexError, TypeError):
             widgets = [widgets]
-    if expand_dependencies in ('full', 'partial'):
-        dependents = expand_dependencies == 'full'
-        state = dependency_state(widgets, drop_defaults, dependents=dependents)
+    if include_dependencies:
+        state = dependency_state(widgets, drop_defaults)
     else:
         state = Widget.get_manager_state(drop_defaults=drop_defaults, widgets=widgets)['state']
 
@@ -180,14 +170,14 @@ def embed_data(widgets, expand_dependencies='full', drop_defaults=True):
 
 
 def embed_snippet(widgets,
-                  expand_dependencies='full',
+                  include_dependencies=True,
                   drop_defaults=True,
                   indent=2,
                   embed_url=None,
                  ):
     """Return a snippet that can be embedded in an HTML file. """
 
-    data = embed_data(widgets, expand_dependencies, drop_defaults)
+    data = embed_data(widgets, include_dependencies, drop_defaults)
 
     widget_views = '\n'.join(
         widget_view_template.format(**dict(view_spec=json.dumps(view_spec)))
