123df034dd26adff7af4e72cbab26524f8681907
Jason Grout <jgrout6@bloomberg.net>
2017-05-31 08:25:11 -0700

    Clean up typescript compilation.

--- jupyter-widgets-base/src/index.ts
+++ jupyter-widgets-base/src/index.ts
@@ -6,21 +6,4 @@ export * from "./utils";
 export * from "./widget";
 export * from "./widget_layout";
 export * from "./widget_style";
-export * from "./widget_link";
-export * from "./widget_bool";
-export * from "./widget_button";
-export * from "./widget_box";
-export * from "./widget_image";
-export * from "./widget_color";
-export * from "./widget_date";
 export * from "./services-shim";
-export * from "./widget_int";
-export * from "./widget_float";
-export * from "./widget_controller";
-export * from "./widget_selection";
-export * from "./widget_selectioncontainer";
-export * from "./widget_string";
-export * from "./widget_description";
-
-export
-const version = (require('../package.json') as any).version;
