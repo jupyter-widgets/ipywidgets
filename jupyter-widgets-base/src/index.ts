e363b83302c8295f87a1bcda91831177470df1d9
Jason Grout <jgrout6@bloomberg.net>
2017-06-01 07:25:23 -0700

    Don’t export utils functions
    We have conflicts with jupyter-js-widgets versions of these functions, and they should probably be considered internal.

--- jupyter-widgets-base/src/index.ts
+++ jupyter-widgets-base/src/index.ts
@@ -2,7 +2,6 @@
 // Distributed under the terms of the Modified BSD License.
 
 export * from "./manager-base";
-export * from "./utils";
 export * from "./widget";
 export * from "./widget_layout";
 export * from "./widget_style";
