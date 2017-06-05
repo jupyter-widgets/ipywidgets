5dea8b7e85489bbfd719fe81b897d8277305604a
Pascal Bugnion <pascal@bugnion.org>
2017-06-05 06:41:00 +0100

    Actually display widget output

--- jupyter-widgets-htmlmanager/src/output.ts
+++ jupyter-widgets-htmlmanager/src/output.ts
@@ -1,9 +1,17 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
 
-import {
-    DOMWidgetModel, DOMWidgetView
-} from '@jupyter-widgets/controls';
+import { DOMWidgetModel, DOMWidgetView } from '@jupyter-widgets/controls';
+
+import { Panel, Widget } from '@phosphor/widgets';
+
+import { OutputAreaModel, OutputArea } from '@jupyterlab/outputarea';
+
+import { IRenderMime, RenderMime } from '@jupyterlab/rendermime';
+
+import { HTMLManager } from './htmlmanager';
+
+import * as $ from 'jquery';
 
 export class OutputModel extends DOMWidgetModel {
     defaults() {
@@ -14,10 +22,50 @@ export class OutputModel extends DOMWidgetModel {
             msg_id: ''
         }
     }
+
+    initialize(attributes: any, options: any) {
+        super.initialize(attributes, options);
+        this._outputs = new OutputAreaModel({
+            values: attributes.outputs
+        });
+    }
+
+    get outputs() {
+        return this._outputs;
+    }
+
+    private _outputs: OutputAreaModel;
+    widget_manager: HTMLManager;
 }
 
 export class OutputView extends DOMWidgetView {
-    render() {
-        console.log('hello view')
+    _createElement(tagName: string) {
+        this.pWidget = new Panel();
+        return this.pWidget.node;
     }
+
+    _setElement(el: HTMLElement) {
+        if (this.el || el !== this.pWidget.node) {
+            // Boxes don't allow setting the element beyond the initial creation.
+            throw new Error('Cannot reset the DOM element.');
+        }
+        this.el = this.pWidget.node;
+        this.$el = $(this.pWidget.node)
+    }
+
+    render() {
+        this._outputView = new OutputArea({
+            rendermime: new RenderMime({items: RenderMime.getDefaultItems()}),
+            contentFactory: OutputArea.defaultContentFactory,
+            model: this.model.outputs
+        })
+        this.pWidget.insertWidget(0, this._outputView);
+        this.pWidget.addClass('jupyter-widgets');
+        this.pWidget.addClass('widget-output');
+        this.update();
+    }
+
+    model: OutputModel;
+    private _outputView: OutputArea;
+    pWidget: Panel;
 }
