80678fea787e7351433fdefcbf002068ace838f7
Pascal Bugnion <pascal@bugnion.org>
2017-06-05 07:20:27 +0100

    Ability to render nested widgets

--- jupyter-widgets-htmlmanager/src/output.ts
+++ jupyter-widgets-htmlmanager/src/output.ts
@@ -15,6 +15,53 @@ import * as $ from 'jquery';
 
 import '../css/output.css';
 
+const WIDGET_MIMETYPE = 'application/vnd.jupyter.widget-view+json';
+
+export class WidgetRenderer implements RenderMime.IRenderer {
+    constructor(widget_manager: HTMLManager) {
+        this._manager = widget_manager;
+    }
+
+    canRender(options: RenderMime.IRenderOptions): boolean {
+        const source: any = options.model.data.get(options.mimeType);
+        const model = this._manager.get_model(source.model_id);
+        return model !== void 0;
+    }
+
+    wouldSanitize(options: RenderMime.IRenderOptions): boolean {
+        return false;
+    }
+
+    render(options: RenderMime.IRenderOptions) {
+        const widget = new Panel();
+        const source: any = options.model.data.get(options.mimeType);
+        const model = this._manager.get_model(source.model_id);
+        console.log(model);
+        if (model) {
+            model.then((model: any) => {
+                return this._manager.display_model(null, model, {
+                    el: widget.node
+                })
+            }).catch((error) => {
+                console.log('Error creating widget.')
+                console.log(error);
+                const node = document.createElement('p')
+                node.textContent = 'Error creating widget.'
+                widget.addWidget(new Widget({node}))
+            })
+        } else {
+            const node = document.createElement('p')
+            node.textContent = 'Error creating widget.'
+            widget.addWidget(new Widget({node}))
+        }
+        return widget;
+    }
+
+    private _manager: HTMLManager;
+    public mimeTypes = [WIDGET_MIMETYPE];
+}
+
+
 export class OutputModel extends DOMWidgetModel {
     defaults() {
         return {
@@ -56,8 +103,13 @@ export class OutputView extends DOMWidgetView {
     }
 
     render() {
+        const rendermime = new RenderMime({items: RenderMime.getDefaultItems()})
+        rendermime.addRenderer({
+            mimeType: WIDGET_MIMETYPE,
+            renderer: new WidgetRenderer(this.model.widget_manager)
+        });
         this._outputView = new OutputArea({
-            rendermime: new RenderMime({items: RenderMime.getDefaultItems()}),
+            rendermime: rendermime,
             contentFactory: OutputArea.defaultContentFactory,
             model: this.model.outputs
         })
