fb8a479b789f28b9bd661f931ea3392f2385514e
Pascal Bugnion <pascal@bugnion.org>
2017-06-10 10:57:10 +0100

    Test for displaying dataframes

--- jupyter-widgets-htmlmanager/test/src/output_test.ts
+++ jupyter-widgets-htmlmanager/test/src/output_test.ts
@@ -53,3 +53,33 @@ describe('text output', () => {
         expect(el.textContent).to.equal(textValue)
     });
 })
+
+
+describe('data output', () => {
+    let view;
+    const textValue = 'this-is-a-test\n'
+
+    // Pandas dataframe
+    const modelState = {
+        outputs: [
+            {
+                "output_type": "display_data",
+                "data": {
+                    "text/plain": "   a  b\n0  1  4\n1  2  5\n2  3  6",
+                    "text/html": "<div>\n<table border=\"1\" class=\"dataframe\">\n  <thead>\n    <tr style=\"text-align: right;\">\n      <th></th>\n      <th>a</th>\n      <th>b</th>\n    </tr>\n  </thead>\n  <tbody>\n    <tr>\n      <th>0</th>\n      <td>1</td>\n      <td>4</td>\n    </tr>\n    <tr>\n      <th>1</th>\n      <td>2</td>\n      <td>5</td>\n    </tr>\n    <tr>\n      <th>2</th>\n      <td>3</td>\n      <td>6</td>\n    </tr>\n  </tbody>\n</table>\n</div>"
+                },
+                "metadata": {}
+            }
+        ],
+    }
+
+    beforeEach(async function() {
+        view = await newWidget(modelState)
+    })
+
+    it('display the dataframe', () => {
+        const outputView = (view as OutputView);
+        const el = view.el;
+        expect(el.querySelectorAll('table').length).to.equal(1);
+    });
+})
