56f184c99b1a30cf0668dbf892f148e48708a14e
Pascal Bugnion <pascal@bugnion.org>
2017-06-10 10:48:12 +0100

    Factor out test fixture

--- jupyter-widgets-htmlmanager/test/src/output_test.ts
+++ jupyter-widgets-htmlmanager/test/src/output_test.ts
@@ -6,37 +6,41 @@ import { OutputModel, OutputView } from '../../lib/output';
 
 import * as widgets from '@jupyter-widgets/controls'
 
-describe('text output', () => {
+const newWidget = async (modelState) => {
+    const widgetTag = document.createElement('div');
+    widgetTag.className = 'widget-subarea';
+    document.body.appendChild(widgetTag);
+    const manager = new HTMLManager()
+    const modelId = 'u-u-i-d';
+    const modelCreate: widgets.ModelOptions = {
+        model_name: 'OutputModel',
+        model_id: modelId,
+        model_module: '@jupyter-widgets/controls',
+        model_module_version: '*'
+    }
+    const model = await manager.new_model(modelCreate, modelState);
+    const view = await manager.display_model(
+        undefined, model, { el: widgetTag }
+    );
+    return view;
+}
 
-    let model: widgets.WidgetModel;
+
+describe('text output', () => {
     let view;
-    let manager: HTMLManager;
+    const textValue = 'this-is-a-test\n'
+    const modelState = {
+        outputs: [
+            {
+                "output_type": "stream",
+                "name": "stdout",
+                "text": textValue
+            }
+        ],
+    }
 
     beforeEach(async function() {
-        const widgetTag = document.createElement('div');
-        widgetTag.className = 'widget-subarea';
-        document.body.appendChild(widgetTag);
-        manager = new HTMLManager()
-        const modelId = 'u-u-i-d';
-        const modelCreate: widgets.ModelOptions = {
-            model_name: 'OutputModel',
-            model_id: modelId,
-            model_module: '@jupyter-widgets/controls',
-            model_module_version: '*'
-        }
-        const modelState = {
-            outputs: [
-                {
-                    "output_type": "stream",
-                    "name": "stdout",
-                    "text": "this-is-a-test\n"
-                }
-            ],
-        }
-        model = await manager.new_model(modelCreate, modelState);
-        view = await manager.display_model(
-            undefined, model, { el: widgetTag }
-        );
+        view = await newWidget(modelState)
     })
 
     it('create the view', () => {
@@ -46,6 +50,6 @@ describe('text output', () => {
     it('display the view', () => {
         const outputView = (view as OutputView);
         const el = view.el;
-        expect(el.textContent).to.equal('this-is-a-test\n')
+        expect(el.textContent).to.equal(textValue)
     });
 })
