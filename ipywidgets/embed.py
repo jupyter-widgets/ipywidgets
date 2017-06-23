0e14be44a776e1f881b93df542ed659a2623a01c
Vidar Tonaas Fauske <vidartf@gmail.com>
2017-06-23 12:52:19 +0200

    Relicense code from ipyvolume
    https://github.com/jupyter-widgets/ipywidgets/pull/1387#issuecomment-310433484
    "consider all code in this PR as a new contribution or consider the code
    to be relicensed under bsd."

--- ipywidgets/embed.py
+++ ipywidgets/embed.py
@@ -1,30 +1,5 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
-#
-#
-# Parts of this code is copied from IPyVolume (24.05.2017), under the following license:
-#
-# MIT License
-#
-# Copyright (c) 2016 Maarten Breddels
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
 
 """
 Functions for generating embeddable HTML/javascript of a widget.
