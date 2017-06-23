2a3a0d2b470833024e077b56641dbf19f6e70047
Jason Grout <jgrout6@bloomberg.net>
2017-06-23 09:31:47 -0400

    Consolidate travis scripts at the repo level.

--- jupyter-widgets-htmlmanager/scripts/travis_script_js.sh
+++
@@ -1,9 +0,0 @@
-set -ex
-export CHROME_BIN=chromium-browser
-export DISPLAY=:99.0
-source ~/.nvm/nvm.sh
-nvm use "v$TRAVIS_NODE_VERSION"
-nvm alias default "v$TRAVIS_NODE_VERSION"
-sh -e /etc/init.d/xvfb start
-cd jupyter-widgets-htmlmanager
-npm run test:unit:$BROWSER
