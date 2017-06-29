jupyter-widgets/ipywidgets

commit 7b9ea6d1bb7be1386f6f67e3e1009c73271fa973

Refactor notebook widgets to use new base class

widgetsnbextension/src/manager.js
@@ -6,6 +6,7 @@ var _ = require("underscore");
 var Backbone = require("backbone");
 var base = require("@jupyter-widgets/base");
 var widgets = require("@jupyter-widgets/controls");
+var outputWidgets = require("@jupyter-widgets/output");
 var saveState = require("./save_state");
 var embedWidgets = require("./embed_widgets");
 var version = require("../package.json").version;
@@ -95,13 +96,11 @@ WidgetManager._managers = []; /* List of widget managers */
 
 WidgetManager.prototype.loadClass = function(className, moduleName, moduleVersion) {
     if (moduleName === "@jupyter-widgets/controls") {
-        if (className === "OutputModel" || className === "OutputView") {
-            return Promise.resolve(output[className]);
-        } else {
-            return Promise.resolve(widgets[className]);
-        }
+        return Promise.resolve(widgets[className]);
     } else if (moduleName === "@jupyter-widgets/base") {
         return Promise.resolve(base[className]);
+    } else if (moduleName == "@jupyter-widgets/output") {
+        return Promise.resolve(outputWidgets[className]);
     } else {
         return new Promise(function(resolve, reject) {
             window.require([moduleName], resolve, reject);

ipywidgets/widgets/widget_output.py
@@ -36,6 +36,8 @@ class Output(DOMWidget, CoreWidget):
     """
     _view_name = Unicode('OutputView').tag(sync=True)
     _model_name = Unicode('OutputModel').tag(sync=True)
+    _view_module = Unicode('@jupyter-widgets/output').tag(sync=True)
+    _model_module = Unicode('@jupyter-widgets/output').tag(sync=True)
     msg_id = Unicode('', help="Parent message id of messages to capture").tag(sync=True)
     outputs = Tuple(help="The output messages synced from the frontend.").tag(sync=True)
 

widgetsnbextension/src/widget_output.js
@@ -5,6 +5,7 @@
 // This widget is strongly coupled to the notebook because of the outputarea
 // dependency.
 var widgets = require("@jupyter-widgets/controls");
+var outputWidgets = require("@jupyter-widgets/output");
 var _ = require("underscore");
 require('./widget_output.css');
 
@@ -12,12 +13,10 @@ var outputArea = new Promise(function(resolve, reject) {
         requirejs(["notebook/js/outputarea"], resolve, reject)
 });
 
-var OutputModel = widgets.DOMWidgetModel.extend({
-    defaults: _.extend({}, widgets.DOMWidgetModel.prototype.defaults, {
-        _model_name: "OutputModel",
-        _view_name: "OutputView",
+var OutputModel = outputWidgets.OutputModel.extend({
+    defaults: _.extend({}, outputWidgets.OutputModel.prototype.defaults(), {
         msg_id: "",
-        outputs: [],
+        outputs: []
     }),
 
     initialize: function(attributes, options) {
@@ -100,7 +99,7 @@ var OutputModel = widgets.DOMWidgetModel.extend({
 
 });
 
-var OutputView = widgets.DOMWidgetView.extend({
+var OutputView = outputWidgets.OutputView.extend({
     render: function(){
         var that = this;
         this.el.classList.add('jupyter-widgets-output-area');

commit dec1f75d0176f7f3564768553bd244d19eba0f33

Fix module loading for output widget in notebook
Commit 7b9ea6d introduced a bug in the notebook extension: instead of loading
the overriden implementation of the output widget, we load the base
implementation (which is mostly empty).

widgetsnbextension/src/manager.js
@@ -6,7 +6,7 @@ var _ = require("underscore");
 var Backbone = require("backbone");
 var base = require("@jupyter-widgets/base");
 var widgets = require("@jupyter-widgets/controls");
-var outputWidgets = require("@jupyter-widgets/output");
+var outputWidgets = require("./widget_output");
 var saveState = require("./save_state");
 var embedWidgets = require("./embed_widgets");
 var version = require("../package.json").version;