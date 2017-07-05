578b6a85b95bf487597f3a6f89dc96c809518848
Jason Grout <jgrout6@bloomberg.net>
2017-07-05 14:29:48 -0400

    ipywidgets 7.0.0a9, widgetsnbextension 3.0.0a12

--- ipywidgets/_version.py
+++ ipywidgets/_version.py
@@ -1,7 +1,7 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (7, 0, 0, 'alpha', 8)
+version_info = (7, 0, 0, 'alpha', 9)
 
 _specifier_ = {'alpha': 'a', 'beta': 'b', 'candidate': 'rc', 'final': ''}
 
--- widgetsnbextension/widgetsnbextension/_version.py
+++ widgetsnbextension/widgetsnbextension/_version.py
@@ -1,4 +1,4 @@
-version_info = (3, 0, 0, 'alpha', 11)
+version_info = (3, 0, 0, 'alpha', 12)
 
 _specifier_ = {'alpha': 'a', 'beta': 'b', 'candidate': 'rc', 'final': ''}
 
