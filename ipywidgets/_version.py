2aaeadd9e6840a01879c03f0d460423779202b99
Jason Grout <jgrout6@bloomberg.net>
2017-07-10 12:30:08 -0500

    ipywidgets 7.0.0a11

--- ipywidgets/_version.py
+++ ipywidgets/_version.py
@@ -1,7 +1,7 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (7, 0, 0, 'alpha', 10)
+version_info = (7, 0, 0, 'alpha', 11)
 
 _specifier_ = {'alpha': 'a', 'beta': 'b', 'candidate': 'rc', 'final': ''}
 
