1f77604fd811eade0f1a8d33e089ac8dcd67afcb
Pascal Bugnion <pascal@bugnion.org>
2017-07-12 07:42:27 +0100

    Test signal triggered correctly

--- packages/controls/test/src/phosphor/currentselection_test.ts
+++ packages/controls/test/src/phosphor/currentselection_test.ts
@@ -1,13 +1,18 @@
 
 import { expect } from 'chai';
+import { spy } from 'sinon';
 
 import { Selection } from '../../../lib/phosphor/currentselection'
 
 describe('Selection with items', function() {
     let selection;
+    let subscriber; // subscribe to signals from selection
+
     beforeEach(function() {
         selection = new Selection(['value-0', 'value-1'])
         selection.index = null;
+        subscriber = spy()
+        selection.selectionChanged.connect(subscriber);
     });
 
     it('be unselected', function() {
@@ -19,5 +24,17 @@ describe('Selection with items', function() {
         selection.index = 1;
         expect(selection.index).to.equal(1);
         expect(selection.value).to.equal('value-1')
+    })
+
+    it('dispatch a signal when setting an item', function() {
+        selection.index = 1;
+        expect(subscriber.calledOnce).to.be.true;
+        const [_, message] = subscriber.getCall(0).args
+        expect(message).to.deep.equal({
+            previousIndex: null,
+            previousValue: null,
+            currentIndex: 1,
+            currentValue: 'value-1'
+        })
     })
 });
