18829a7713169cb7fdd7a1ed10881743913b85d8
Jason Grout <jgrout6@bloomberg.net>
2017-07-14 11:53:54 -0500

    Register the output plugin in jlab with the correct module and version.
    Fixes #1504

--- packages/output/src/output.ts
+++ packages/output/src/output.ts
@@ -1,7 +1,10 @@
 
 import {
-    DOMWidgetModel, DOMWidgetView
+    DOMWidgetModel, DOMWidgetView, JUPYTER_WIDGETS_VERSION
 } from '@jupyter-widgets/base';
+
+export
+const OUTPUT_WIDGET_VERSION = JUPYTER_WIDGETS_VERSION;
 
 export class OutputModel extends DOMWidgetModel {
     defaults() {
@@ -10,7 +13,9 @@ export class OutputModel extends DOMWidgetModel {
             _model_name: 'OutputModel',
             _view_name: 'OutputView',
             _model_module: '@jupyter-widgets/output',
-            _view_module: '@jupyter-widgets/output'
+            _view_module: '@jupyter-widgets/output',
+            _model_module_version: OUTPUT_WIDGET_VERSION,
+            _view_module_version: OUTPUT_WIDGET_VERSION,
         }
     }
 }
