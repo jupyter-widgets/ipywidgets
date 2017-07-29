43eddc3663f64e92a673c09ce52ddcd50b935842
Pascal Bugnion <pascal@bugnion.org>
2017-07-29 10:37:48 +0100

    Test that the float slider uses the NumberFormat traitlet

---
+++ ipywidgets/widgets/tests/test_widget_float.py
@@ -0,0 +1,20 @@
+
+from unittest import TestCase
+
+from traitlets import TraitError
+
+from ipywidgets import FloatSlider
+
+
+class TestFloatSlider(TestCase):
+
+    def test_construction(self):
+        FloatSlider()
+
+    def test_construction_readout_format(self):
+        slider = FloatSlider(readout_format='$.1f')
+        assert slider.get_state()['readout_format'] == '$.1f'
+
+    def test_construction_invalid_readout_format(self):
+        with self.assertRaises(TraitError):
+            FloatSlider(readout_format='broken')
