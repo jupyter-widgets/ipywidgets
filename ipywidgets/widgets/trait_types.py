b5fd45517a9fb5f0248e9d505f113956ee0c0289
Pascal Bugnion <pascal@bugnion.org>
2017-07-29 10:04:41 +0100

    More explicit error message

--- ipywidgets/widgets/trait_types.py
+++ ipywidgets/widgets/trait_types.py
@@ -9,6 +9,7 @@ import re
 import traitlets
 import datetime as dt
 
+
 _color_names = ['aliceblue', 'antiquewhite', 'aqua', 'aquamarine', 'azure', 'beige', 'bisque', 'black', 'blanchedalmond', 'blue', 'blueviolet', 'brown', 'burlywood', 'cadetblue', 'chartreuse', 'chocolate', 'coral', 'cornflowerblue', 'cornsilk', 'crimson', 'cyan', 'darkblue', 'darkcyan', 'darkgoldenrod', 'darkgray', 'darkgreen', 'darkkhaki', 'darkmagenta', 'darkolivegreen', 'darkorange', 'darkorchid', 'darkred', 'darksalmon', 'darkseagreen', 'darkslateblue', 'darkslategray', 'darkturquoise', 'darkviolet', 'deeppink', 'deepskyblue', 'dimgray', 'dodgerblue', 'firebrick', 'floralwhite', 'forestgreen', 'fuchsia', 'gainsboro', 'ghostwhite', 'gold', 'goldenrod', 'gray', 'green', 'greenyellow', 'honeydew', 'hotpink', 'indianred ', 'indigo ', 'ivory', 'khaki', 'lavender', 'lavenderblush', 'lawngreen', 'lemonchiffon', 'lightblue', 'lightcoral', 'lightcyan', 'lightgoldenrodyellow', 'lightgray', 'lightgreen', 'lightpink', 'lightsalmon', 'lightseagreen', 'lightskyblue', 'lightslategray', 'lightsteelblue', 'lightyellow', 'lime', 'limegreen', 'linen', 'magenta', 'maroon', 'mediumaquamarine', 'mediumblue', 'mediumorchid', 'mediumpurple', 'mediumseagreen', 'mediumslateblue', 'mediumspringgreen', 'mediumturquoise', 'mediumvioletred', 'midnightblue', 'mintcream', 'mistyrose', 'moccasin', 'navajowhite', 'navy', 'oldlace', 'olive', 'olivedrab', 'orange', 'orangered', 'orchid', 'palegoldenrod', 'palegreen', 'paleturquoise', 'palevioletred', 'papayawhip', 'peachpuff', 'peru', 'pink', 'plum', 'powderblue', 'purple', 'rebeccapurple', 'red', 'rosybrown', 'royalblue', 'saddlebrown', 'salmon', 'sandybrown', 'seagreen', 'seashell', 'sienna', 'silver', 'skyblue', 'slateblue', 'slategray', 'snow', 'springgreen', 'steelblue', 'tan', 'teal', 'thistle', 'tomato', 'turquoise', 'violet', 'wheat', 'white', 'whitesmoke', 'yellow', 'yellowgreen']
 _color_re = re.compile(r'#[a-fA-F0-9]{3}(?:[a-fA-F0-9]{3})?$')
 
@@ -154,7 +155,7 @@ class NumberFormat(traitlets.Unicode):
     TODO
     """
 
-    info_text = 'A valid number format'
+    info_text = 'a valid number format'
     default_value = traitlets.Undefined
 
     def validate(self, obj, value):
@@ -164,11 +165,14 @@ class NumberFormat(traitlets.Unicode):
             self.error(obj, value)
         else:
             format_type = re_match.group(9)
-            print format_type
             if format_type is None:
                 return value
             elif format_type in _number_format_types:
                 return value
             else:
-                self.error(obj, value)
-        return value
+                raise traitlets.TraitError(
+                    'The type specifier of a NumberFormat trait must '
+                    'be one of {}, but a value of \'{}\' was '
+                    'specified.'.format(
+                        list(_number_format_types), format_type)
+                )
