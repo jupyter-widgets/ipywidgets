1afcddbc6360a730cbc0da23ddbc5b815838bb27
Jason Grout <jgrout6@bloomberg.net>
2017-07-31 16:52:20 -0400

    Export the @jupyter-widgets/base and @jupyter-widgets/controls modules in the html manager.

--- packages/html-manager/src/index.ts
+++ packages/html-manager/src/index.ts
@@ -3,3 +3,15 @@
 
 export * from "./embed-helper";
 export * from "./htmlmanager";
+
+// We make the base @jupyter-widgets/base and @jupyter-widgets/controls modules
+// available so that other modules can require them.
+
+window['requirejs'].config({
+    map: {
+        '*': {
+            '@jupyter-widgets/base': 'base',
+            '@jupyter-widgets/controls': 'controls',
+        },
+    }
+});
--- packages/html-manager/webpack.config.js
+++ packages/html-manager/webpack.config.js
@@ -4,16 +4,27 @@
 var version = require('./package.json').version;
 
 var postcss = require('postcss');
-module.exports = {
-    entry: './lib/embed-webpack.js',
-    output: {
-        filename : 'index.js',
-        path: './dist',
-        publicPath: 'https://unpkg.com/@jupyter-widgets/htmlmanager@' + version + '/dist/'
-    },
-    devtool: 'source-map',
-    module: {
-        loaders: [
+
+var postcssHandler = () => {
+    return [
+        postcss.plugin('delete-tilde', () => {
+            return function (css) {
+                css.walkAtRules('import', (rule) => {
+                    rule.params = rule.params.replace('~', '');
+                });
+            };
+        }),
+        postcss.plugin('prepend', () => {
+            return (css) => {
+                css.prepend(`@import '@jupyter-widgets/controls/css/labvariables.css';`)
+            }
+        }),
+        require('postcss-import')(),
+        require('postcss-cssnext')()
+    ];
+}
+
+var loaders = [
             { test: /\.css$/, loader: "style-loader!css-loader!postcss-loader" },
             { test: /\.json$/, loader: "json-loader" },
             // jquery-ui loads some images
@@ -25,23 +36,46 @@ module.exports = {
             { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
             { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" }
         ]
+
+module.exports = [
+{// base manager
+    entry: './lib/embed-webpack.js',
+    output: {
+        filename : 'index.js',
+        path: './dist',
+        publicPath: 'https://unpkg.com/@jupyter-widgets/htmlmanager@' + version + '/dist/',
+        libraryTarget: 'amd'
     },
-    postcss: () => {
-        return [
-            postcss.plugin('delete-tilde', () => {
-                return function (css) {
-                    css.walkAtRules('import', (rule) => {
-                        rule.params = rule.params.replace('~', '');
-                    });
-                };
-            }),
-            postcss.plugin('prepend', () => {
-                return (css) => {
-                    css.prepend(`@import '@jupyter-widgets/controls/css/labvariables.css';`)
-                }
-            }),
-            require('postcss-import')(),
-            require('postcss-cssnext')()
-        ];
-    }
-};
+    devtool: 'source-map',
+    module: { loaders: loaders },
+    postcss: postcssHandler,
+    externals: ['@jupyter-widgets/base', '@jupyter-widgets/controls']
+},
+{// @jupyter-widgets/base
+    entry: '@jupyter-widgets/base/lib/index',
+    output: {
+        //library: '@jupyter-widgets/base',
+        filename : 'base.js',
+        path: './dist',
+        publicPath: 'https://unpkg.com/@jupyter-widgets/base@' + version + '/dist/',
+        libraryTarget: 'amd',
+    },
+    devtool: 'source-map',
+    module: { loaders: loaders },
+    postcss: postcssHandler
+},
+{// @jupyter-widgets/controls
+    entry: '@jupyter-widgets/controls/lib/index',
+    output: {
+        //library: '@jupyter-widgets/controls',
+        filename : 'controls.js',
+        path: './dist',
+        publicPath: 'https://unpkg.com/@jupyter-widgets/controls@' + version + '/dist/',
+        libraryTarget: 'amd'
+    },
+    devtool: 'source-map',
+    module: { loaders: loaders },
+    postcss: postcssHandler,
+    externals: ['@jupyter-widgets/base']
+}
+];
