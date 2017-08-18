a440bd726a3f43946acb693024064bf1e149aa39
Min RK <benjaminrk@gmail.com>
2017-08-18 13:08:11 +0200

    instantiate IPython when testing _ipython_display_
    if IPython isn't instantiated, display calls print (IPython master)

--- ipywidgets/widgets/tests/test_widget.py
+++ ipywidgets/widgets/tests/test_widget.py
@@ -3,16 +3,22 @@
 
 """Test Widget."""
 
-from IPython.utils.capture import capture_output
+from IPython.core.interactiveshell import InteractiveShell
 from IPython.display import display
+from IPython.utils.capture import capture_output
 
 from ..widget import Widget
 
 
 def test_no_widget_view():
+    # ensure IPython shell is instantiated
+    # otherwise display() just calls print
+    shell = InteractiveShell.instance()
+
     with capture_output() as cap:
         w = Widget()
         display(w)
-    assert len(cap.outputs) == 0
-    assert len(cap.stdout) == 0
-    assert len(cap.stderr) == 0
+
+    assert cap.outputs == [], repr(cap.outputs)
+    assert cap.stdout == '', repr(cap.stdout)
+    assert cap.stderr == '', repr(cap.stderr)
