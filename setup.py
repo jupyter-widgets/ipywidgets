472e8e9ff4c2fd9940a0bb7217bcf934242e0004
maartenbreddels <maartenbreddels@gmail.com>
2017-08-29 16:47:28 +0200

    wrong path for sample movie

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ setup_args = dict(
     scripts         = [],
     packages        = packages,
     package_data    = {
-        'ipywidgets': [ 'state.schema.json', 'view.schema.json', 'ipywidgets/widgets/tests/data/jupyter.mp4']
+        'ipywidgets': [ 'state.schema.json', 'view.schema.json', 'widgets/tests/data/jupyter.mp4']
     },
     description     = "IPython HTML widgets for Jupyter",
     long_description = LONG_DESCRIPTION,
