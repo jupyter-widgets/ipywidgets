ca5352dfffed9daaea1799b3e26ed8739d88c3fc
Pascal Bugnion <pascal@bugnion.org>
2017-09-18 08:03:16 +0100

    Missing file headers

--- ipywidgets/widgets/docutils.py
+++ ipywidgets/widgets/docutils.py
@@ -1,3 +1,5 @@
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
 
 def doc_subst(snippets):
     """ Substitute format strings in class or function docstring """
