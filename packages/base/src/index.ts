37023072e58f21cf1a3bc170834a7ebc944aadf8
Craig Nishina <craig.nishina@gmail.com>
2017-10-13 18:41:15 -0700

    tslint for packages/base/src
    - Override the tslint interface rule since it enforces that the interface
      starts with an 'I'.
    - Override the tslint no bitwise rule  since it is used in the utils.ts
      file.
    - Override the no-invalid-this rule when not in a class. The 'this' keyword
      appears in the backbone-patch.ts.

--- packages/base/src/index.ts
+++ packages/base/src/index.ts
@@ -1,10 +1,10 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
 
-export * from "./manager-base";
-export * from "./widget";
-export * from "./widget_layout";
-export * from "./widget_style";
-export * from "./services-shim";
-export * from "./viewlist";
-export * from "./utils";
+export * from './manager-base';
+export * from './widget';
+export * from './widget_layout';
+export * from './widget_style';
+export * from './services-shim';
+export * from './viewlist';
+export * from './utils';
