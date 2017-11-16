ab1bb552ce17dca20176eda3c72436bb0e1c8d3f
Steven Silvester <steven.silvester@ieee.org>
2017-11-16 03:56:08 -0600

    update ci to use yarn

--- scripts/travis_install_js.sh
+++ scripts/travis_install_js.sh
@@ -1,9 +1,5 @@
-source ~/.nvm/nvm.sh
-nvm install "v$TRAVIS_NODE_VERSION"
-nvm use "v$TRAVIS_NODE_VERSION"
-nvm alias default "v$TRAVIS_NODE_VERSION"
-echo `npm -v`
+echo `yarn -v`
 echo `node -v`
-npm install
-npm run build
-npm run build:examples
+yarn
+yarn run build
+yarn run build:examples
