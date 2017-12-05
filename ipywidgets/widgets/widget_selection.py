97a4ded708c673e97e76ea29101995e52833faae
Jason Grout <jgrout6@bloomberg.net>
2017-12-05 13:22:27 -0800

    Revert the button_color argument from 7f2af46c178760b7bfca5a91e5352ac5714a9d54
    The toggle button color changes if it is active or not. I thought about adding another `button_active_color`, but we don’t have a good way of having dynamic styles applied (i.e., the css is dynamically applied instead of hardcoded on the DOM element when set).

--- ipywidgets/widgets/widget_selection.py
+++ ipywidgets/widgets/widget_selection.py
@@ -17,7 +17,7 @@ from .widget_description import DescriptionWidget, DescriptionStyle
 from .valuewidget import ValueWidget
 from .widget_core import CoreWidget
 from .widget_style import Style
-from .trait_types import InstanceDict, Color
+from .trait_types import InstanceDict
 from .widget import register, widget_serialization
 from .docutils import doc_subst
 from traitlets import (Unicode, Bool, Int, Any, Dict, TraitError, CaselessStrEnum,
@@ -386,21 +386,13 @@ class ToggleButtonsStyle(DescriptionStyle, CoreWidget):
         The width of each button. This should be a valid CSS
         width, e.g. '10px' or '5em'.
 
-    button_color: str or None
-        The color of each button. This should be a valid CSS color unit,
-        for example 'yellow' or '#FFFF00'.
-
     font_weight: str
         The text font weight of each button, This should be a valid CSS font
         weight unit, for example 'bold' or '600'
     """
     _model_name = Unicode('ToggleButtonsStyleModel').tag(sync=True)
     button_width = Unicode(help="The width of each button.").tag(sync=True)
-    button_color = Color(None, allow_none=True,
-                         help="Color of each button.").tag(sync=True)
-    font_weight = Unicode(
-        help="Text font weight of each button."
-    ).tag(sync=True)
+    font_weight = Unicode(help="Text font weight of each button.").tag(sync=True)
 
 
 @register
