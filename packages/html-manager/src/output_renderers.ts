fc042775880d14ed38346a1db6bfdf2b23618c36
Jason Grout <jgrout6@bloomberg.net>
2017-12-08 20:51:37 -0800

    Preserve the Phosphor widget hierarchy when rendering widgets in the html manager.

--- packages/html-manager/src/output_renderers.ts
+++ packages/html-manager/src/output_renderers.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
 
-import { Widget, Panel } from '@phosphor/widgets';
+import { Widget, Panel, PanelLayout } from '@phosphor/widgets';
 
 import { IRenderMime } from '@jupyterlab/rendermime-interfaces';
 
@@ -15,6 +15,7 @@ export class WidgetRenderer extends Widget implements IRenderMime.IRenderer {
         super();
         this.mimeType = options.mimeType;
         this._manager = manager;
+        this.layout = new PanelLayout();
     }
 
     async renderModel(model: IRenderMime.IMimeModel) {
@@ -22,9 +23,15 @@ export class WidgetRenderer extends Widget implements IRenderMime.IRenderer {
         const modelPromise = this._manager.get_model(source.model_id);
         if (modelPromise) {
             try {
+                // We don't call 'display_model' or 'display_view' because we
+                // want to preserve the Phosphor parent/child relationship. This
+                // is consistent with display_model/view as long as they only
+                // attach the widget to the DOM.
                 let wModel = await modelPromise;
-                await this._manager.display_model(null, wModel, {el: this.node});
-            } catch(err) {
+                let view = await this._manager.create_view(wModel);
+                let layout = this.layout as PanelLayout;
+                layout.addWidget(view.pWidget);
+            } catch (err) {
                 console.log('Error displaying widget');
                 console.log(err);
                 this.node.textContent = 'Error displaying widget';
