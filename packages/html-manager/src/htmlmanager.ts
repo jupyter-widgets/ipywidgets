938bde8667c77684ed591cee13c52217311a6f15
Jason Grout <jgrout6@bloomberg.net>
2017-12-08 19:11:38 -0800

    Clean up html manager class

--- packages/html-manager/src/htmlmanager.ts
+++ packages/html-manager/src/htmlmanager.ts
@@ -8,15 +8,15 @@ import * as outputWidgets from './output';
 import * as PhosphorWidget from '@phosphor/widgets';
 import { RenderMime, defaultRendererFactories } from '@jupyterlab/rendermime';
 
-import { OutputModel, OutputView } from './output'
-import { WidgetRenderer, WIDGET_MIMETYPE } from './output_renderers'
+import { OutputModel, OutputView } from './output';
+import { WidgetRenderer, WIDGET_MIMETYPE } from './output_renderers';
 
 export
 class HTMLManager extends base.ManagerBase<HTMLElement> {
 
-    constructor(options?: {loader?: (moduleName: string, moduleVersion: string) => Promise<any>}) {
+    constructor(options?: WidgetManager.IOptions) {
         super();
-        this.loader = options && options.loader;
+        this._loader = options && options.loader;
         this.renderMime = new RenderMime({
             initialFactories: defaultRendererFactories
         });
@@ -26,6 +26,7 @@ class HTMLManager extends base.ManagerBase<HTMLElement> {
             createRenderer: (options) => new WidgetRenderer(options, this)
         }, 0);
     }
+
     /**
      * Display the specified view. Element where the view is displayed
      * is specified in the `options.el` argument.
@@ -38,30 +39,30 @@ class HTMLManager extends base.ManagerBase<HTMLElement> {
             });
             return view;
         });
-    };
+    }
 
     /**
      * Placeholder implementation for _get_comm_info.
      */
     _get_comm_info() {
         return Promise.resolve({});
-    };
+    }
 
     /**
      * Placeholder implementation for _create_comm.
      */
     _create_comm(comm_target_name: string, model_id: string, data?: any, metadata?: any, buffers?: ArrayBuffer[] | ArrayBufferView[]): Promise<any> {
         return Promise.resolve({
-            on_close: () => {},
-            on_msg: () => {},
-            close: () => {}
+            on_close: () => { return; },
+            on_msg: () => { return; },
+            close: () => { return; }
         });
-    };
+    }
 
     /**
      * Load a class and return a promise to the loaded object.
      */
-    protected loadClass(className: string, moduleName: string, moduleVersion: string) {
+    protected loadClass(className: string, moduleName: string, moduleVersion: string): Promise<typeof base.WidgetModel | typeof base.WidgetView> {
         return new Promise((resolve, reject) => {
             if (moduleName === '@jupyter-widgets/base') {
                 resolve(base);
@@ -69,8 +70,8 @@ class HTMLManager extends base.ManagerBase<HTMLElement> {
                 resolve(widgets);
             } else if (moduleName === '@jupyter-widgets/output') {
                 resolve(outputWidgets);
-            } else if (this.loader !== undefined) {
-                resolve(this.loader(moduleName, moduleVersion))
+            } else if (this._loader) {
+                resolve(this._loader(moduleName, moduleVersion));
             } else {
                 reject(`Could not load module ${moduleName}@${moduleVersion}`);
             }
@@ -88,10 +89,24 @@ class HTMLManager extends base.ManagerBase<HTMLElement> {
      *
      * Defines how outputs in the output widget should be rendered.
      */
-    renderMime: RenderMime
+    renderMime: RenderMime;
 
     /**
      * A loader for a given module name and module version, and returns a promise to a module
      */
-    loader: (moduleName: string, moduleVersion: string) => Promise<any>;
-};
+    private _loader: (moduleName: string, moduleVersion: string) => Promise<any>;
+}
+
+/**
+ * The namespace for the `WidgetManager` class statics.
+ */
+export
+namespace WidgetManager {
+    export
+    interface IOptions {
+        /**
+         * The loader to load custom widget modules.
+         */
+        loader?: (moduleName: string, moduleVersion: string) => Promise<any>;
+    }
+}
