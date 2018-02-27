c03e45dec3dfe7adcd972145af171eed98d7f326
Sylvain Corlay <sylvain.corlay@gmail.com>
2018-02-27 20:18:24 +0100

    Use execute_result instead of display_data message for initial display

--- ipywidgets/widgets/widget.py
+++ ipywidgets/widgets/widget.py
@@ -697,7 +697,7 @@ class Widget(LoggingHasTraits):
         """Convert json values to objects."""
         return x
 
-    def _ipython_display_(self, **kwargs):
+    def _repr_mimebundle_(self, **kwargs):
         """Called when `IPython.display.display` is called on the widget."""
         if self._view_name is not None:
 
@@ -715,9 +715,8 @@ class Widget(LoggingHasTraits):
                     'model_id': self._model_id
                 }
             }
-            display(data, raw=True)
-
             self._handle_displayed(**kwargs)
+            return data
 
     def _send(self, msg, buffers=None):
         """Sends a message to the model in the front-end."""
