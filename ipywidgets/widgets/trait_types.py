117cee8b8432c27321e1776736c2d24616dfa2b0
Jason Grout <jasongrout@users.noreply.github.com>
2018-03-20 10:06:22 -0700

    Add TypedTuple docstring

--- ipywidgets/widgets/trait_types.py
+++ ipywidgets/widgets/trait_types.py
@@ -185,5 +185,6 @@ class NumberFormat(traitlets.Unicode):
                 )
 
 class TypedTuple(traitlets.Container):
+    """A trait for a tuple of any length with type-checked elements."""
     klass = tuple
     _cast_types = (list,)
