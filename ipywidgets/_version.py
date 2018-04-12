e1af98674e46c93c1fffe1e42a20adb2a62dad1e
Jason Grout <jgrout6@bloomberg.net>
2018-04-12 15:41:04 -0700

    ipywidgets 7.2.1, widgetsnbextension 3.2.1
    This upgrades to the newest jupyterlab services package.
    
    ipywidgets-7.2.1.tar.gz 
    md5: 4eefd1b210020f27c8e384f50f98b9f8
    sha1: 4f20d3394f026060ad374c1613d008a85846586c
    sha256: ab9869cda5af7ba449d8f707b29b7e97a7db97d6366805d6b733338f51096f54
    
    widgetsnbextension-3.2.1.tar.gz 
    md5: 2b87b1461738f10c0d476b2e2aa8099d
    sha1: 62f0a262bb36bd8fc25332707ba76bdf6f79b4b6
    sha256: 5417789ee6064ff515fd10be24870660af3561c02d3d48b26f6f44285d0f70cc

--- ipywidgets/_version.py
+++ ipywidgets/_version.py
@@ -1,7 +1,7 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (7, 2, 0, 'final', 0)
+version_info = (7, 2, 1, 'final', 0)
 
 _specifier_ = {'alpha': 'a', 'beta': 'b', 'candidate': 'rc', 'final': ''}
 
@@ -14,4 +14,4 @@ __jupyter_widgets_output_version__ = '1.0.0'
 __jupyter_widgets_controls_version__ = '1.2.0'
 
 # A compatible @jupyter-widgets/html-manager npm package semver range
-__html_manager_version__ = '^0.12.0'
+__html_manager_version__ = '^0.13.0'
