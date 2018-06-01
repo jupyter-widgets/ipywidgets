a35c86e26ea5790f39d70a519fd8adb56761c8f2
Vidar Tonaas Fauske <vidartf@gmail.com>
2018-06-01 09:44:32 -0700

    Change how buffers serialize

--- ipywidgets/widgets/widget.py
+++ ipywidgets/widgets/widget.py
@@ -10,6 +10,7 @@ in the IPython notebook front-end.
 from contextlib import contextmanager
 import collections
 import sys
+import numbers
 
 from IPython.core.getipython import get_ipython
 from ipykernel.comm import Comm
@@ -56,78 +57,30 @@ if PY3:
 else:
     _binary_types = (memoryview, bytearray)
 
-def _put_buffers(state, buffer_paths, buffers):
-    """The inverse of _remove_buffers, except here we modify the existing dict/lists.
-    Modifying should be fine, since this is used when state comes from the wire.
-    """
-    for buffer_path, buffer in zip(buffer_paths, buffers):
-        # we'd like to set say sync_data['x'][0]['y'] = buffer
-        # where buffer_path in this example would be ['x', 0, 'y']
-        obj = state
-        for key in buffer_path[:-1]:
-            obj = obj[key]
-        obj[buffer_path[-1]] = buffer
-
-def _separate_buffers(substate, path, buffer_paths, buffers):
-    """For internal, see _remove_buffers"""
-    # remove binary types from dicts and lists, but keep track of their paths
-    # any part of the dict/list that needs modification will be cloned, so the original stays untouched
-    # e.g. {'x': {'ar': ar}, 'y': [ar2, ar3]}, where ar/ar2/ar3 are binary types
-    # will result in {'x': {}, 'y': [None, None]}, [ar, ar2, ar3], [['x', 'ar'], ['y', 0], ['y', 1]]
-    # instead of removing elements from the list, this will make replacing the buffers on the js side much easier
-    if isinstance(substate, (list, tuple)):
-        is_cloned = False
-        for i, v in enumerate(substate):
-            if isinstance(v, _binary_types):
-                if not is_cloned:
-                    substate = list(substate) # shallow clone list/tuple
-                    is_cloned = True
-                substate[i] = None
-                buffers.append(v)
-                buffer_paths.append(path + [i])
-            elif isinstance(v, (dict, list, tuple)):
-                vnew = _separate_buffers(v, path + [i], buffer_paths, buffers)
-                if v is not vnew: # only assign when value changed
-                    if not is_cloned:
-                        substate = list(substate) # clone list/tuple
-                        is_cloned = True
-                    substate[i] = vnew
-    elif isinstance(substate, dict):
-        is_cloned = False
-        for k, v in substate.items():
-            if isinstance(v, _binary_types):
-                if not is_cloned:
-                    substate = dict(substate) # shallow clone dict
-                    is_cloned = True
-                del substate[k]
-                buffers.append(v)
-                buffer_paths.append(path + [k])
-            elif isinstance(v, (dict, list, tuple)):
-                vnew = _separate_buffers(v, path + [k], buffer_paths, buffers)
-                if v is not vnew: # only assign when value changed
-                    if not is_cloned:
-                        substate = dict(substate) # clone list/tuple
-                        is_cloned = True
-                    substate[k] = vnew
+def _binary_to_json(x, obj, buffers):
+    # TODO: Serialize lists/dicts of buffers
+    if isinstance(x, _binary_types):
+        buffers.append(x)
+        return len(buffers)
+    elif isinstance(x, numbers.Number):
+        # Avoid passing numbers through, as they will mess up deserialization
+        raise TypeError('Cannot serialize a number as a binary type!')
     else:
-        raise ValueError("expected state to be a list or dict, not %r" % substate)
-    return substate
+        return x
+
+def _json_to_binary(x, obj, buffers):
+    # TODO: Deserialize lists/dicts of buffers
+    if isinstance(x, numbers.Number):
+        return buffers[x]
+    else:
+        return x
+
+binary_serialization = {
+    'from_json': _json_to_binary,
+    'to_json': _binary_to_json
+}
 
 
-def _remove_buffers(state):
-    """Return (state_without_buffers, buffer_paths, buffers) for binary message parts
-
-    A binary message part is a memoryview, bytearray, or python 3 bytes object.
-
-    As an example:
-    >>> state = {'plain': [0, 'text'], 'x': {'ar': memoryview(ar1)}, 'y': {'shape': (10,10), 'data': memoryview(ar2)}}
-    >>> _remove_buffers(state)
-    ({'plain': [0, 'text']}, {'x': {}, 'y': {'shape': (10, 10)}}, [['x', 'ar'], ['y', 'data']],
-     [<memory at 0x107ffec48>, <memory at 0x107ffed08>])
-    """
-    buffer_paths, buffers = [], []
-    state = _separate_buffers(state, [], buffer_paths, buffers)
-    return state, buffer_paths, buffers
 
 def _buffer_list_equal(a, b):
     """Compare two lists of buffers for equality.
