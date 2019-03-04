bc0a5eb7de4fbb83052661c20650383c486d3669
Bartosz Telenczuk <bartosz.telenczuk@gmail.com>
2019-03-04 13:01:10 +0100

    adding applayout traitlet widgets

--- ipywidgets/widgets/widget_templates.py
+++ ipywidgets/widgets/widget_templates.py
@@ -4,22 +4,29 @@ from .widget import register
 from .widget_button import Button, ButtonStyle
 from .widget_layout import Layout
 
-@register
+from traitlets import Instance
+
 class AppLayout(Widget):
+
+    left_top = Instance(Widget)
 
     def __init__(self, **kwargs):
         super(AppLayout, self).__init__(**kwargs)
-        self._box = GridBox()
 
-        self._box = GridBox(children=[Button(layout=Layout(width='auto', height='auto'),
-                                 style=ButtonStyle(button_color='darkseagreen')) for i in range(9)
-                         ],
-                layout=Layout(
-                    width='50%',
-                    grid_template_columns='100px 50px 100px',
-                    grid_template_rows='80px auto 80px',
-                    grid_gap='5px 10px')
-               )
+        left_top = self.left_top
+        left_bottom = Button(layout=Layout(width='auto', height='auto'))
+        right_top = Button(layout=Layout(width='auto', height='auto'))
+        right_bottom = Button(layout=Layout(width='auto', height='auto'))
+
+        grid_layout = Layout(grid_template_columns='50% 50%',
+                             grid_template_rows='50% 50%')
+
+        self._box = GridBox(children=[left_top,
+                                      right_top,
+                                      left_bottom,
+                                      right_bottom],
+                            layout=grid_layout)
+
     def _ipython_display_(self, **kwargs):
 
         # delegate to GridBox class
