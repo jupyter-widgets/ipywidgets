0b6154551d97cf88901fc40ae3e8d9350781db62
Bartosz Telenczuk <bartosz.telenczuk@gmail.com>
2019-03-08 11:01:21 +0100

    add gridspeclayout (first version)

--- ipywidgets/widgets/tests/test_widget_templates.py
+++ ipywidgets/widgets/tests/test_widget_templates.py
@@ -548,6 +548,89 @@ class TestAppLayout(TestCase):
         assert box.layout.grid_template_rows == "3fr 100px 20fr"
         assert box.layout.grid_template_columns == "3fr 3fr 1fr"
 
+class TestGridspecLayout(TestCase):
+    "test GridspecLayout"
+
+    def test_init(self):
+        with pytest.raises(traitlets.TraitError):
+            box = widgets.GridspecLayout()
+
+        with pytest.raises(traitlets.TraitError):
+            box = widgets.GridspecLayout(n_rows=-1, n_columns=1)
+
+        box = widgets.GridspecLayout(n_rows=5, n_columns=3)
+        assert box.n_rows == 5
+        assert box.n_columns == 3
+        assert len(box._grid_template_areas) == 5
+        assert len(box._grid_template_areas[0]) == 3
+
+        box = widgets.GridspecLayout(1, 2)
+        assert box.n_rows == 1
+        assert box.n_columns == 2
+
+        with pytest.raises(traitlets.TraitError):
+            box = widgets.GridspecLayout(0, 0)
+
+    def test_setitem_index(self):
+
+        box = widgets.GridspecLayout(2, 3)
+        button1 = widgets.Button()
+        button2 = widgets.Button()
+        button3 = widgets.Button()
+        button4 = widgets.Button()
+
+        box[0, 0] = button1
+        button1_label = button1.layout.grid_area
+        assert button1 in box.children
+        assert box.layout.grid_template_areas == '''"{} . ."\n". . ."'''.format(button1_label)
+
+        box[-1, -1] = button2
+        button2_label = button2.layout.grid_area
+        assert button1_label != button2_label
+        assert button2 in box.children
+        assert box.layout.grid_template_areas == '''"{} . ."\n". . {}"'''.format(button1_label,
+                                                                                 button2_label)
+
+        box[1, 0] = button3
+        button3_label = button3.layout.grid_area
+        assert button1_label != button3_label
+        assert button2_label != button3_label
+        assert button3 in box.children
+        assert box.layout.grid_template_areas == '''"{b1} . ."\n"{b3} . {b2}"'''.format(b1=button1_label,
+                                                                                        b2=button2_label,
+                                                                                        b3=button3_label)
+
+        #replace widget
+        box[1, 0] = button4
+        button4_label = button4.layout.grid_area
+        assert button1_label != button4_label
+        assert button2_label != button4_label
+        assert button4 in box.children
+        assert button3 not in box.children
+
+        assert box.layout.grid_template_areas == '''"{b1} . ."\n"{b4} . {b2}"'''.format(b1=button1_label,
+                                                                                        b2=button2_label,
+                                                                                        b4=button4_label)
+
+
+    def test_getitem_index(self):
+        "test retrieving widget"
+
+        box = widgets.GridspecLayout(2, 3)
+        button1 = widgets.Button()
+        button2 = widgets.Button()
+        button3 = widgets.Button()
+        button4 = widgets.Button()
+
+        box[0, 0] = button1
+
+        assert box[0, 0] is button1
+
+
+
+
+
+
 
 class TestLayoutProperties(TestCase):
     """test mixin with layout properties"""
--- ipywidgets/widgets/widget_templates.py
+++ ipywidgets/widgets/widget_templates.py
@@ -1,8 +1,9 @@
 """Implement common widgets layouts as reusable components"""
 
 from traitlets import Instance, Bool, Unicode, CUnicode, CaselessStrEnum, Tuple
-from traitlets import HasTraits
-from traitlets import observe
+from traitlets import Integer
+from traitlets import HasTraits, TraitError
+from traitlets import observe, validate
 
 from .widget import Widget
 from .widget_box import GridBox
@@ -248,6 +249,77 @@ class AppLayout(GridBox, LayoutProperties):
 
     @observe("footer", "header", "center", "left_sidebar", "right_sidebar", "merge",
              "pane_widths", "pane_heights")
+    def _child_changed(self, change): #pylint: disable=unused-argument
+        self._update_layout()
+
+
+@doc_subst(_doc_snippets)
+class GridspecLayout(GridBox, LayoutProperties):
+    """ Define a N by M grid layout
+
+    Parameters
+    ----------
+
+
+
+    {style_params}
+
+    Examples
+    --------
+
+    """
+
+    n_rows = Integer()
+    n_columns = Integer()
+    # extra args
+
+    def __init__(self, n_rows=None, n_columns=None, **kwargs):
+        super(GridspecLayout, self).__init__(**kwargs)
+        self.n_rows = n_rows
+        self.n_columns = n_columns
+        self._grid_template_areas = [['.'] * self.n_columns for i in range(self.n_rows)]
+
+        self._grid_template_rows = 'repeat(%d, %.1f%%)' % (self.n_rows, 100./self.n_rows)
+        self._grid_template_columns = 'repeat(%d, %.1f%%)' % (self.n_columns, 100./self.n_columns)
+        self._children = {}
+        self._id_count = 0
+
+    @validate('n_rows', 'n_columns')
+    def _validate_integer(self, proposal):
+        if proposal['value'] > 0:
+            return proposal['value']
+        raise TraitError('n_rows and n_columns must be positive integer')
+
+
+    def __setitem__(self, key, value):
+        row, column = key
+        self._id_count += 1
+        obj_id = 'widget%03d' % self._id_count
+        value.layout.grid_area = obj_id
+        current_value = self._grid_template_areas[row][column]
+        if current_value != '.':
+            del self._children[current_value]
+        self._grid_template_areas[row][column] = obj_id
+        self._children[obj_id] = value
+        self._update_layout()
+
+    def __getitem__(self, key):
+        row, column = key
+        obj_id = self._grid_template_areas[row][column]
+        return self._children[obj_id]
+
+
+    def _update_layout(self):
+
+        grid_template_areas_css = "\n".join('"{}"'.format(" ".join(line))
+                                            for line in self._grid_template_areas)
+
+        self.layout.grid_template_columns = self._grid_template_columns
+        self.layout.grid_template_rows = self._grid_template_rows
+        self.layout.grid_template_areas = grid_template_areas_css
+        self.children = tuple(self._children.values())
+
+
     def _child_changed(self, change): #pylint: disable=unused-argument
         self._update_layout()
 
