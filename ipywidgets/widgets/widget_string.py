3336a0a7214e13fcaaa0ab52ac29ac552d70622d
Bartosz Telenczuk <bartosz.telenczuk@gmail.com>
2019-04-11 12:17:50 +0200

    adding ondrop handler

--- ipywidgets/widgets/widget_string.py
+++ ipywidgets/widgets/widget_string.py
@@ -57,6 +57,43 @@ class Label(_String):
     _model_name = Unicode('LabelModel').tag(sync=True)
     draggable = Bool(default=False).tag(sync=True)
 
+    def __init__(self, *args, **kwargs):
+        super(Label, self).__init__(*args, **kwargs)
+        self._click_handlers = CallbackDispatcher()
+        self.on_msg(self._handle_drop_msg)
+
+    def on_drop(self, callback, remove=False):
+        """Register a callback to execute when an element is dropped.
+
+        The callback will be called with two arguments, the clicked button
+        widget instance, and the dropped element data.
+
+        Parameters
+        ----------
+        remove: bool (optional)
+            Set to true to remove the callback from the list of callbacks.
+        """
+        self._click_handlers.register_callback(callback, remove=remove)
+
+    def drop(self, data):
+        """Programmatically trigger a drop event.
+
+        This will call the callbacks registered to the  drop event.
+        """
+
+        self._click_handlers(self, data)
+
+    def _handle_drop_msg(self, _, content, buffers):
+        """Handle a msg from the front-end.
+
+        Parameters
+        ----------
+        content: dict
+            Content of the msg.
+        """
+        if content.get('event', '') == 'drop':
+            self.drop(content.get('data', {}))
+
 
 @register
 class Textarea(_String):
