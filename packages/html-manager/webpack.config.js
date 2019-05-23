5c0b0e09212c50b263c0ad20cb5e707922181b6a
Sylvain Corlay <sylvain.corlay@gmail.com>
2019-05-23 17:21:38 +0200

    Drop postcss

--- packages/html-manager/webpack.config.js
+++ packages/html-manager/webpack.config.js
@@ -7,33 +7,10 @@ var path = require('path');
 
 var version = require('./package.json').version;
 
-var postcss = require('postcss');
-
 var rules = [
     { test: /\.css$/, use: [
         'style-loader',
-        'css-loader',
-        {
-            loader: 'postcss-loader',
-            options: {
-                plugins: [
-                    postcss.plugin('delete-tilde', function() {
-                        return function (css) {
-                            css.walkAtRules('import', function(rule) {
-                                rule.params = rule.params.replace('~', '');
-                            });
-                        };
-                    }),
-                    postcss.plugin('prepend', function() {
-                        return function(css) {
-                            css.prepend("@import '@jupyter-widgets/controls/css/labvariables.css';")
-                        }
-                    }),
-                    require('postcss-import')(),
-                    require('postcss-cssnext')()
-                ]
-            }
-        }
+        'css-loader'
     ]},
     // jquery-ui loads some images
     { test: /\.(jpg|png|gif)$/, use: 'file-loader' },
