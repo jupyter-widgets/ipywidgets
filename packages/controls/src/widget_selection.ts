9fe66d8f6b68e62c2983d6840fcc2a2e0f82cf69
Zerline <odile.benassy@u-psud.fr>
2019-11-26 14:20:05 +0100

    Tabindex for Select and Dropdown.

--- packages/controls/src/widget_selection.ts
+++ packages/controls/src/widget_selection.ts
@@ -30,6 +30,48 @@ class SelectionModel extends CoreDescriptionModel {
 }
 
 export
+class SelectionView extends DescriptionView {
+    /**
+     * Called when view is rendered.
+     */
+    render() {
+        super.render(); // Incl. setting some defaults.
+        this.el.classList.add('jupyter-widgets');
+        this.el.classList.add('widget-inline-hbox');
+    }
+
+    /**
+     * Update the contents of this view
+     *
+     * Called when the model is changed.  The model may have been
+     * changed by another view or by a state update from the back-end.
+     */
+    update() {
+        super.update();
+
+        // Disable listbox if needed
+        if (this.listbox) {
+            this.listbox.disabled = this.model.get('disabled');
+        }
+
+        // Set tabindex
+        this.updateTabindex();
+    }
+
+    updateTabindex() {
+        if (!this.listbox) return; // we might be constructing the parent
+        let tabindex = this.model.get('tabindex');
+        if (tabindex === null) {
+            this.listbox.removeAttribute('tabindex');
+        } else {
+        this.listbox.setAttribute('tabindex', tabindex);
+        }
+    }
+
+    listbox: HTMLSelectElement;
+}
+
+export
 class DropdownModel extends SelectionModel {
     defaults() {
         return {...super.defaults(),
@@ -48,7 +90,7 @@ class DropdownModel extends SelectionModel {
 // For the old code, see commit f68bfbc566f3a78a8f3350b438db8ed523ce3642
 
 export
-class DropdownView extends DescriptionView {
+class DropdownView extends SelectionView {
     /**
      * Public constructor.
      */
@@ -63,8 +105,6 @@ class DropdownView extends DescriptionView {
     render() {
         super.render();
 
-        this.el.classList.add('jupyter-widgets');
-        this.el.classList.add('widget-inline-hbox');
         this.el.classList.add('widget-dropdown');
 
         this.listbox = document.createElement('select');
@@ -78,9 +118,6 @@ class DropdownView extends DescriptionView {
      * Update the contents of this view
      */
     update() {
-        // Disable listbox if needed
-        this.listbox.disabled = this.model.get('disabled');
-
         // Select the correct element
         let index = this.model.get('index');
         this.listbox.selectedIndex = index === null ? -1 : index;
@@ -113,8 +150,6 @@ class DropdownView extends DescriptionView {
         this.model.set('index', this.listbox.selectedIndex === -1 ? null : this.listbox.selectedIndex);
         this.touch();
     }
-
-    listbox: HTMLSelectElement;
 }
 
 
@@ -131,7 +166,7 @@ class SelectModel extends SelectionModel {
 }
 
 export
-class SelectView extends DescriptionView {
+class SelectView extends SelectionView {
     /**
      * Public constructor.
      */
@@ -148,9 +183,6 @@ class SelectView extends DescriptionView {
      */
     render() {
         super.render();
-
-        this.el.classList.add('jupyter-widgets');
-        this.el.classList.add('widget-inline-hbox');
         this.el.classList.add('widget-select');
 
         this.listbox.id = this.label.htmlFor = uuid();
@@ -165,7 +197,6 @@ class SelectView extends DescriptionView {
      */
     update() {
         super.update();
-        this.listbox.disabled = this.model.get('disabled');
         let rows = this.model.get('rows');
         if (rows === null) {
             rows = '';
@@ -207,8 +238,6 @@ class SelectView extends DescriptionView {
         this.model.set('index', this.listbox.selectedIndex, {updated_view: this});
         this.touch();
     }
-
-    listbox: HTMLSelectElement;
 }
 
 export
@@ -233,8 +262,6 @@ class RadioButtonsView extends DescriptionView {
     render() {
         super.render();
 
-        this.el.classList.add('jupyter-widgets');
-        this.el.classList.add('widget-inline-hbox');
         this.el.classList.add('widget-radio');
 
         this.container = document.createElement('div');
@@ -401,8 +428,6 @@ class ToggleButtonsView extends DescriptionView {
     render() {
         super.render();
 
-        this.el.classList.add('jupyter-widgets');
-        this.el.classList.add('widget-inline-hbox');
         this.el.classList.add('widget-toggle-buttons');
 
         this.buttongroup = document.createElement('div');
@@ -586,8 +611,6 @@ class SelectionSliderView extends DescriptionView {
     render () {
         super.render();
 
-        this.el.classList.add('jupyter-widgets');
-        this.el.classList.add('widget-inline-hbox');
         this.el.classList.add('widget-hslider');
         this.el.classList.add('widget-slider');
 
