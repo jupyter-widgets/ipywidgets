3c348cce13aff07ce78251f485bd0ff82f8e4e15
Jason Grout <jgrout6@bloomberg.net>
2020-01-06 18:45:35 +0100

    Delete travis tests that are duplicated by github actions.
    We leave the one test that github actions does not test.

--- scripts/travis_script_js.sh
+++ scripts/travis_script_js.sh
@@ -1,22 +1,4 @@
 set -ex
-export CHROME_BIN=chromium-browser
-
-yarn run integrity
-
-cd packages
-
-cd base
-yarn run test:unit:$BROWSER
-cd ..
-
-cd controls
-yarn run test:unit:$BROWSER
-cd ..
-
-cd html-manager
-yarn run test:unit:$BROWSER
-cd ..
-cd ..
 
 cd examples/web1
 yarn run test:firefox
