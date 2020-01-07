b561f1f72675419d58f1a31de709020b066a2490
Jason Grout <jgrout6@bloomberg.net>
2020-01-07 16:53:25 +0100

    Rename phosphor files and directories to lumino.

--- packages/controls/src/widget_selectioncontainer.ts
+++ packages/controls/src/widget_selectioncontainer.ts
@@ -15,11 +15,11 @@ import {
 
 import {
     TabPanel
-} from './phosphor/tabpanel';
+} from './lumino/tabpanel';
 
 import {
     Accordion
-} from './phosphor/accordion';
+} from './lumino/accordion';
 
 import {
     Widget
