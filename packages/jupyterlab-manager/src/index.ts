3df0763bdeffd17165334479e080752931e54cb2
martinRenou <martin.renou@gmail.com>
2020-01-08 16:56:22 +0100

    Add Font Awesome 5.12 to the JupyterLab Manager
    Signed-off-by: martinRenou <martin.renou@gmail.com>

--- packages/jupyterlab-manager/src/index.ts
+++ packages/jupyterlab-manager/src/index.ts
@@ -1,6 +1,9 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
 
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import '@fortawesome/fontawesome-free/css/v4-shims.min.css';
+
 import WidgetManagerProvider from './plugin';
 import * as output from './output';
 
