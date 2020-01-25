3658ff7788fded711fae903c6ec86daa7f642d16
Pascal Bugnion <pascal@bugnion.org>
2020-01-25 11:34:46 +0000

    Test construction options for file upload

--- packages/controls/test/src/widget_upload_test.ts
+++ packages/controls/test/src/widget_upload_test.ts
@@ -29,6 +29,22 @@ describe.only('FileUploadView', function() {
     );
   });
 
+  function fileInputForModel(model: widgets.FileUploadModel) {
+    // For a given model, create and render a view and return the
+    // view's input.
+    const options = { model };
+    const view = new widgets.FileUploadView(options);
+    view.render();
+    return getFileInput(view);
+  }
+
+  function proxyButtonForModel(model: widgets.FileUploadModel) {
+    const options = { model };
+    const view = new widgets.FileUploadView(options);
+    view.render();
+    return getProxyButton(view);
+  }
+
   it('construction', function() {
     const options = { model: this.model };
     const view = new widgets.FileUploadView(options);
@@ -44,16 +60,44 @@ describe.only('FileUploadView', function() {
     expect(fileInput.disabled).to.be.false;
     expect(fileInput.multiple).to.be.false;
     expect(proxyButton.innerText).to.equal('Upload (0)');
-    expect(proxyButton.querySelector('i')).to.not.be.undefined;
+    expect(proxyButton.querySelector('i')).to.not.be.null;
     expect(proxyButton.querySelector('i')!.className).to.equal('fa fa-upload');
   });
 
   it('multiple', function() {
-    const options = { model: this.model };
     this.model.set('multiple', true);
-    const view = new widgets.FileUploadView(options);
-    view.render();
-    const fileInput = getFileInput(view);
+    const fileInput = fileInputForModel(this.model);
     expect(fileInput.multiple).to.be.true;
+  });
+
+  it('accept', function() {
+    this.model.set('accept', 'text/csv');
+    const fileInput = fileInputForModel(this.model);
+    expect(fileInput.accept).to.equal('text/csv');
+  });
+
+  it('disabled', function() {
+    this.model.set('disabled', true);
+    const proxyButton = proxyButtonForModel(this.model);
+    expect(proxyButton.disabled).to.be.true;
+  });
+
+  it('no icon', function() {
+    this.model.set('icon', '');
+    const proxyButton = proxyButtonForModel(this.model);
+    expect(proxyButton.querySelector('i')).to.be.null;
+  });
+
+  it('other icon', function() {
+    this.model.set('icon', 'check');
+    const proxyButton = proxyButtonForModel(this.model);
+    expect(proxyButton.querySelector('i')).to.not.be.null;
+    expect(proxyButton.querySelector('i')!.className).to.equal('fa fa-check');
+  });
+
+  it('description', function() {
+    this.model.set('description', 'some text');
+    const proxyButton = proxyButtonForModel(this.model);
+    expect(proxyButton.innerText).to.equal('some text (0)');
   });
 });
