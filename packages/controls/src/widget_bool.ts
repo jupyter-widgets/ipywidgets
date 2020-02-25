bc7566f04bf2efb6dab36a3866468d39f72d7fe1
Jason Grout <jgrout6@bloomberg.net>
2020-02-25 11:13:48 -0800

    Change fontawesome support to work for fontawesome 4 and 5.
    JupyterLab and notebook are upgrading to fontawesome 5 (with the v4 backwards compatibility shim). However, in some places, our CSS is very specific to version 4. This commit modifies these places to use syntax that should for both fontawesome 4 and 5. It involves some DOM structure changes, but that is a private implementation detail, so is backwards compatible with our public API.
    
    This is a forward-port of https://github.com/jupyter-widgets/ipywidgets/pull/2793 and 5941cba85fb0959572d59af5301b34d89e18e653
    
    Fixes #2794

--- packages/controls/src/widget_bool.ts
+++ packages/controls/src/widget_bool.ts
@@ -301,13 +301,14 @@ export class ValidView extends DescriptionView {
   /**
    * Called when view is rendered.
    */
-  render(): void {
+  render() {
     super.render();
     this.el.classList.add('jupyter-widgets');
     this.el.classList.add('widget-valid');
     this.el.classList.add('widget-inline-hbox');
-    const icon = document.createElement('i');
-    this.el.appendChild(icon);
+    this.icon = document.createElement('i');
+    this.icon.classList.add('fa', 'fa-fw');
+    this.el.appendChild(this.icon);
     this.readout = document.createElement('span');
     this.readout.classList.add('widget-valid-readout');
     this.readout.classList.add('widget-readout');
@@ -321,15 +322,20 @@ export class ValidView extends DescriptionView {
    * Called when the model is changed.  The model may have been
    * changed by another view or by a state update from the back-end.
    */
-  update(): void {
+  update() {
     this.el.classList.remove('mod-valid');
     this.el.classList.remove('mod-invalid');
+    this.icon.classList.remove('fa-check');
+    this.icon.classList.remove('fa-times');
     this.readout.textContent = this.model.get('readout');
     if (this.model.get('value')) {
       this.el.classList.add('mod-valid');
+      this.icon.classList.add('fa-check');
     } else {
       this.el.classList.add('mod-invalid');
+      this.icon.classList.add('fa-times');
     }
   }
   readout: HTMLSpanElement;
+  icon: HTMLElement;
 }
