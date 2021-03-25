76a313b2e15ced1f79d5a2de250c94ddbc867b72
Mehmet Bektas <mbektas@bloomberg.net>
2021-03-25 12:05:51 -0700

    add reference images

--- ui-tests/tests/widgets.test.ts
+++ ui-tests/tests/widgets.test.ts
@@ -72,7 +72,7 @@ describe('Widget Visual Regression', () => {
     for (let c = 0; c < numCellImages; ++c) {
       expect(
         await galata.capture.compareScreenshot(getCaptureImageName(c))
-      ).toBe('missing-reference');
+      ).toBe('same');
     }
   });
 
