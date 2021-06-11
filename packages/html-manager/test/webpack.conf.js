d90eabd79b2b94db46d6ea47dcc5366083e84460
Jason Grout <jgrout6@bloomberg.net>
2021-06-11 09:13:31 -0700

    Add ‘process’ definition for blueprint in webpack 5.
    See https://github.com/palantir/blueprint/issues/4393

--- packages/html-manager/test/webpack.conf.js
+++ packages/html-manager/test/webpack.conf.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var postcss = require('postcss');
+const webpack = require('webpack');
 
 module.exports = {
   mode: 'development',
@@ -90,4 +91,12 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      // Needed for Blueprint. See https://github.com/palantir/blueprint/issues/4393
+      'process.env': '{}',
+      // Needed for various packages using cwd(), like the path polyfill
+      process: { cwd: () => '/' }
+    })
+  ]
 };
