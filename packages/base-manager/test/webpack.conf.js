2260f62bf4437ad533572ec2839e83bbfc894fa4
Jason Grout <jgrout6@bloomberg.net>
2021-06-10 22:29:17 -0700

    Fix webpack config for webpack 5

--- packages/base-manager/test/webpack.conf.js
+++ packages/base-manager/test/webpack.conf.js
@@ -10,10 +10,10 @@ module.exports = {
   bail: true,
   module: {
     rules: [
-      { test: /\.css$/, loader: 'style-loader!css-loader' },
-      { test: /\.md$/, loader: 'raw-loader' },
-      { test: /\.html$/, loader: 'file?name=[name].[ext]' },
-      { test: /\.ipynb$/, loader: 'json-loader' }
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      { test: /\.md$/, use: 'raw-loader' },
+      { test: /\.html$/, use: {loader: 'file-loader', options: { name: '[name].[ext]' } } },
+      { test: /\.ipynb$/, use: 'json-loader' }
     ]
   },
   mode: 'development'
