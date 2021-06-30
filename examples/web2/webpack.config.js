e8b6c51f5c6b83e0661bd90819aa4083b33fa69c
Jason Grout <jgrout6@bloomberg.net>
2021-06-29 18:14:11 -0700

    Fix example webpack config to work with jlab dependencies.

--- examples/web2/webpack.config.js
+++ examples/web2/webpack.config.js
@@ -1,4 +1,5 @@
 var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
   mode: 'development',
@@ -57,5 +58,13 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      // Needed for Blueprint. See https://github.com/palantir/blueprint/issues/4393
+      'process.env': '{}',
+      // Needed for various packages using cwd(), like the path polyfill
+      process: { cwd: () => '/' }
+    })
+  ]
 };
