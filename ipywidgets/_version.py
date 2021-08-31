1326c9b0e9dea093f0a7aa80210d607b1d98cb14
Jason Grout <jgrout6@bloomberg.net>
2021-08-31 16:59:34 -0700

    ipywidgets 7.6.4, jupyterlab_widgets 1.0.1
    dist/ipywidgets-7.6.4-py2.py3-none-any.whl
    md5: 478ad5354b08dcabb6a1245c9a1ad197
    sha1: 46b0d6cb9bb6b1a061057afb10a4baf0a3758679
    sha256: 3ffd1baa741eb631e7a3a69d4df290de074ef697e0ef3176e33361b44cd91711
    
    dist/ipywidgets-7.6.4.tar.gz
    md5: 771fd3999f93af336c6e40ae1f5c37a2
    sha1: 8ea8c8e15a4779897dc9f12cc20e29cfe5fb689f
    sha256: 028bf014a0b1d77cb676fe163115f145aacdde0bb9a51c4166940e5b62a7d1d0
    
    jupyterlab_widgets/dist/jupyterlab_widgets-1.0.1-py3-none-any.whl
    md5: db4d1ceefdef6b8d3afb892f501c83de
    sha1: 420e56889416ccbd632a1a32b2278949ddc09f55
    sha256: 841925a349bd9a9197c5506bd5461a321b09e6659a9b179a0096b561a92898c3
    
    jupyterlab_widgets/dist/jupyterlab_widgets-1.0.1.tar.gz
    md5: 6222b4d9f59a0bc5a6b74fe37eb59000
    sha1: 08838b21cdfcf9d686472f476304414a6f5ce1db
    sha256: f94fb7fa1ddc8668e3f98d67a97cabe322e8d04b78b9eb988c7fde415d7a02df

--- ipywidgets/_version.py
+++ ipywidgets/_version.py
@@ -1,7 +1,7 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (7, 6, 3, 'final', 0)
+version_info = (7, 6, 4, 'final', 0)
 
 _specifier_ = {'alpha': 'a', 'beta': 'b', 'candidate': 'rc', 'final': ''}
 
