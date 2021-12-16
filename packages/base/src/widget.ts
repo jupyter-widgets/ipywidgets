da9cb86e30ac9c2cc9a19e03e2db0596d3126227
Mario Buikhuizen <mariobuikhuizen@gmail.com>
2021-12-16 14:29:14 +0100

    fix: echos from other clients are not unexpected

--- packages/base/src/widget.ts
+++ packages/base/src/widget.ts
@@ -233,33 +233,34 @@ export class WidgetModel extends Backbone.Model {
             if (msg.parent_header && data.echo) {
               const msgId = (msg.parent_header as any).msg_id;
               // for echoed attributes we ignore old updates
-              data.echo.forEach((attrName: string) => {
-                if (!Object.keys(this.attrLastUpdateMsgId).includes(attrName)) {
-                  console.error(`Kernel send unexpected echo for ${attrName}.`);
-                }
-                // we don't care about the old messages, only the one send with the
-                // last msgId
-                const isOldMessage =
-                  this.attrLastUpdateMsgId[attrName] !== msgId;
-                if (isOldMessage) {
-                  // get rid of old updates
-                  delete state[attrName];
-                } else {
-                  // we got our confirmation, from now on we accept everything
-                  delete this.attrLastUpdateMsgId[attrName];
-                  // except, we plan to send out a new state for this soon, so we will
-                  // also ignore the update for this property
-                  if (
-                    this._msg_buffer !== null &&
-                    Object.prototype.hasOwnProperty.call(
-                      this._msg_buffer,
-                      attrName
-                    )
-                  ) {
+              data.echo
+                .filter((attrName: string) =>
+                  Object.keys(this.attrLastUpdateMsgId).includes(attrName)
+                )
+                .forEach((attrName: string) => {
+                  // we don't care about the old messages, only the one send with the
+                  // last msgId
+                  const isOldMessage =
+                    this.attrLastUpdateMsgId[attrName] !== msgId;
+                  if (isOldMessage) {
+                    // get rid of old updates
                     delete state[attrName];
+                  } else {
+                    // we got our confirmation, from now on we accept everything
+                    delete this.attrLastUpdateMsgId[attrName];
+                    // except, we plan to send out a new state for this soon, so we will
+                    // also ignore the update for this property
+                    if (
+                      this._msg_buffer !== null &&
+                      Object.prototype.hasOwnProperty.call(
+                        this._msg_buffer,
+                        attrName
+                      )
+                    ) {
+                      delete state[attrName];
+                    }
                   }
-                }
-              });
+                });
             }
             return (this.constructor as typeof WidgetModel)._deserialize_state(
               state,
