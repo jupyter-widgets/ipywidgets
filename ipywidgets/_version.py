d426b3e00d75523a7599474739511407ca69a781
Jason Grout <jason@jasongrout.org>
2022-03-09 17:31:12 -0800

    ipywidgets 7.7.0rc0, widgetsnbextension 3.6.0rc0, jupyterlab_widgets 1.1.0rc0
    dist/ipywidgets-7.7.0rc0-py2.py3-none-any.whl
    md5: e156dd58fcea6030cf51cd87246ccdf6
    sha1: 1e13e24322b40c4d92b1e9f663f9db987b9bc698
    sha256: e8d678071ed6f68948dbddba312d0ca4f590fdf7febc4156cc0b6179731b574c
    
    dist/ipywidgets-7.7.0rc0.tar.gz
    md5: bd3c1326edc7a227c69220123d1f166d
    sha1: d944fd197e0223037cd620d3b389948b58468368
    sha256: cef40a78c68d8d7f5dceb2fa7c1885da99ff7d95927848b51475386bcfc07998
    
    widgetsnbextension/dist/widgetsnbextension-3.6.0rc0-py2.py3-none-any.whl
    md5: 690daf418be353e560c0b7f9a454d1d9
    sha1: 9597a05e5feca295745a888d99cc6ced9dd27222
    sha256: 876fade8036be7dac01c9a52e0284cc2d6050c961219f29d070076fc6652a0f7
    
    widgetsnbextension/dist/widgetsnbextension-3.6.0rc0.tar.gz
    md5: b0819ea9da8121d3bd4b503c615eadba
    sha1: 6a430ee2dbe2618d1f002c21f6b583de9689bafa
    sha256: 365ca7d11b182477f87d00f97d89e40a119a4799dfe4db911d6ac6c5d97de703
    
    jupyterlab_widgets/dist/jupyterlab_widgets-1.1.0rc0-py3-none-any.whl
    md5: 8676a4038ba99559870f59908f2e87d0
    sha1: a9def8082d594736f6148bd27e6af9a58caf85dc
    sha256: e9545d037d2353e82c33e256b04feaff2cefc95164331daeabacb5c8aca01699
    
    jupyterlab_widgets/dist/jupyterlab_widgets-1.1.0rc0.tar.gz
    md5: 6b8d5322d9d67a09a79acaa14a0d3318
    sha1: e5b7960dc99b55c570f545b74e96b77b0d7582e9
    sha256: 7acbeddb7d768547bda5110970091ebe48c110694c92b3d9cd5d1745d3655c30

--- ipywidgets/_version.py
+++ ipywidgets/_version.py
@@ -1,7 +1,7 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (7, 7, 0, 'alpha', 0)
+version_info = (7, 7, 0, 'candidate', 0)
 
 _specifier_ = {'alpha': 'a', 'beta': 'b', 'candidate': 'rc', 'final': ''}
 
--- widgetsnbextension/widgetsnbextension/_version.py
+++ widgetsnbextension/widgetsnbextension/_version.py
@@ -1,4 +1,4 @@
-version_info = (3, 6, 0, 'alpha', 0)
+version_info = (3, 6, 0, 'candidate', 0)
 
 _specifier_ = {'alpha': 'a', 'beta': 'b', 'candidate': 'rc', 'final': ''}
 
