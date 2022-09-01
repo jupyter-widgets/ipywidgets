36add2bf2d7975eccf6d9fc4115a0707fa5cbc34
Vidar Tonaas Fauske <510760+vidartf@users.noreply.github.com>
2022-09-01 18:33:40 +0100

    Add a regression test for time and datetime cross validation
    Also fixes the none check in time validation

--- python/ipywidgets/ipywidgets/widgets/tests/test_widget_datetime.py
+++ python/ipywidgets/ipywidgets/widgets/tests/test_widget_datetime.py
@@ -6,7 +6,9 @@
 
 import pytest
 
+from contextlib import nullcontext
 import datetime
+import itertools
 
 import pytz
 from traitlets import TraitError
@@ -38,6 +40,44 @@ def test_datetime_validate_value_none():
     w = DatetimePicker(value=dt, min=dt_min, max=dt_max)
     w.value = None
     assert w.value is None
+
+
+def _permuted_dts():
+    ret = []
+    combos = list(itertools.product([None, dt_1442, dt_2002, dt_2056], repeat=3))
+    for vals in combos:
+        expected = vals[0]
+        if vals[1] and vals[2] and vals[1] > vals[2]:
+            expected = TraitError
+        elif vals[0] is None:
+            pass
+        elif vals[1] and vals[1] > vals[0]:
+            expected = vals[1]
+        elif vals[2] and vals[2] < vals[0]:
+            expected = vals[2]
+        ret.append(vals + (expected,))
+    return ret
+
+
+@pytest.mark.parametrize(
+    "input_value,input_min,input_max,expected",
+    _permuted_dts()
+)
+def test_datetime_cross_validate_value_min_max(
+    input_value,
+    input_min,
+    input_max,
+    expected,
+):
+    w = DatetimePicker(value=dt_2002, min=dt_2002, max=dt_2002)
+    should_raise = expected is TraitError
+    with pytest.raises(expected) if should_raise else nullcontext():
+        with w.hold_trait_notifications():
+            w.value = input_value
+            w.min = input_min
+            w.max = input_max
+    if not should_raise:
+        assert w.value is expected
 
 
 def test_datetime_validate_value_vs_min():
