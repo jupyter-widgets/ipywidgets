c1c34f9010b40ac3374ae0c6383f64a992207ca7
Maarten A. Breddels <maartenbreddels@gmail.com>
2022-09-29 13:04:32 +0200

    perf: no need to have Comm as a trait
    comm is mostly for internal use, and only causes runtime overhead.

--- ipywidgets/widgets/widget.py
+++ ipywidgets/widgets/widget.py
@@ -456,7 +456,7 @@ class Widget(LoggingHasTraits):
 
     _view_count = Int(None, allow_none=True,
         help="EXPERIMENTAL: The number of views of the model displayed in the frontend. This attribute is experimental and may change or be removed in the future. None signifies that views will not be tracked. Set this to 0 to start tracking view creation/deletion.").tag(sync=True)
-    comm = Instance('ipykernel.comm.Comm', allow_none=True)
+    comm = None
 
     keys = List(help="The traits which are synced.")
 
@@ -506,16 +506,9 @@ class Widget(LoggingHasTraits):
                 args['comm_id'] = self._model_id
 
             self.comm = Comm(**args)
-
-    @observe('comm')
-    def _comm_changed(self, change):
-        """Called when the comm is changed."""
-        if change['new'] is None:
-            return
-        self._model_id = self.model_id
-
-        self.comm.on_msg(self._handle_msg)
-        Widget.widgets[self.model_id] = self
+            self._model_id = self.model_id
+            self.comm.on_msg(self._handle_msg)
+            Widget.widgets[self.model_id] = self
 
     @property
     def model_id(self):
