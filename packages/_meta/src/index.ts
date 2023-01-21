b6f93463ba8cb60d1e1094954dfd570b5b7a1d14
Nicholas Bollweg <nick.bollweg@gmail.com>
2023-01-21 11:50:51 -0600

    start work on docs build, add ts metapackage, more verbose build output

---
+++ packages/_meta/src/index.ts
@@ -0,0 +1,6 @@
+import '@jupyter-widgets/base-manager';
+import '@jupyter-widgets/base';
+import '@jupyter-widgets/controls';
+import '@jupyter-widgets/html-manager';
+import '@jupyter-widgets/jupyterlab-manager';
+import '@jupyter-widgets/output';
