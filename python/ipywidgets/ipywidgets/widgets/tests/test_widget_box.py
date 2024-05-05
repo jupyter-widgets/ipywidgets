70b88bc8387be9133cd54862858be09848fcf10e
Alan Fleming <>
2024-05-05 12:05:38 +1000

    Add test_box_invalid_children convert TestBox to separate pytests.

--- python/ipywidgets/ipywidgets/widgets/tests/test_widget_box.py
+++ python/ipywidgets/ipywidgets/widgets/tests/test_widget_box.py
@@ -3,59 +3,76 @@
 
 import gc
 import weakref
-from unittest import TestCase
 
+import pytest
 from traitlets import TraitError
 
 import ipywidgets as widgets
 
 
-class TestBox(TestCase):
-
-    def test_construction(self):
-        box = widgets.Box()
-        assert box.get_state()['children'] == []
-
-    def test_construction_with_children(self):
-        html = widgets.HTML('some html')
-        slider = widgets.IntSlider()
-        box = widgets.Box([html, slider])
-        children_state = box.get_state()['children']
-        assert children_state == [
-            widgets.widget._widget_to_json(html, None),
-            widgets.widget._widget_to_json(slider, None),
-        ]
-
-    def test_construction_style(self):
-        box = widgets.Box(box_style='warning')
-        assert box.get_state()['box_style'] == 'warning'
-
-    def test_construction_invalid_style(self):
-        with self.assertRaises(TraitError):
-            widgets.Box(box_style='invalid')
+def test_box_construction():
+    box = widgets.Box()
+    assert box.get_state()["children"] == []
 
 
-    def test_gc(self):
-        widgets.enable_weakrefence()
-        # Test Box gc collected and children lifecycle managed.
-        try:
-            deleted = False
-            class TestButton(widgets.Button):
-                def my_click (self, b):
-                    pass
-            button = TestButton(description='button')
-            button.on_click(button.my_click)
-            
-            b = widgets.VBox(children=[button])
+def test_box_construction_with_children():
+    html = widgets.HTML("some html")
+    slider = widgets.IntSlider()
+    box = widgets.Box([html, slider])
+    children_state = box.get_state()["children"]
+    assert children_state == [
+        widgets.widget._widget_to_json(html, None),
+        widgets.widget._widget_to_json(slider, None),
+    ]
 
-            def on_delete():
-                nonlocal deleted
-                deleted = True
 
-            weakref.finalize(b, on_delete)
-            del b
-            gc.collect()
-            assert deleted        
-        finally:
-            widgets.disable_weakrefence()
-        
+def test_box_construction_style():
+    box = widgets.Box(box_style="warning")
+    assert box.get_state()["box_style"] == "warning"
+
+
+def test_construction_invalid_style():
+    with pytest.raises(TraitError):
+        widgets.Box(box_style="invalid")
+
+
+def test_box_invalid_children():
+    box = widgets.Box()
+    closed_button = widgets.Button(description="Closed")
+    closed_button.close()
+
+    with pytest.raises(TypeError):
+        box.children = ["Not a widget"]
+    with pytest.raises(TypeError):
+        box.children = [closed_button]
+    with pytest.raises(TypeError):
+        box.children = [closed_button]
+
+
+def test_box_gc():
+    widgets.VBox._active_widgets
+    widgets.enable_weakrefence()
+    # Test Box gc collected and children lifecycle managed.
+    try:
+        deleted = False
+
+        class TestButton(widgets.Button):
+            def my_click(self, b):
+                pass
+
+        button = TestButton(description="button")
+        button.on_click(button.my_click)
+
+        b = widgets.VBox(children=[button])
+
+        def on_delete():
+            nonlocal deleted
+            deleted = True
+
+        weakref.finalize(b, on_delete)
+        del b
+        gc.collect()
+        assert deleted
+        widgets.VBox._active_widgets
+    finally:
+        widgets.disable_weakrefence()
