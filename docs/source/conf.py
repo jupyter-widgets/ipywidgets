a91f8043188796b86249902040bad041fbef2af6
martinRenou <martin.renou@gmail.com>
2024-05-23 16:59:08 +0200

    Docs: Update jupyterlite (#3918)
    * Docs: Update jupyterlite
    
    * Linter
    
    * Update yarn.lock
    
    * Add links

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -131,7 +131,7 @@ autodoc_default_options = {
 rediraffe_redirects = {
     "typedoc/index": "_static/typedoc/index",
     "try/lab/index": "_static/lab/index",
-    "try/retro/index": "_static/retro/tree/index",
+    "try/notebook/index": "_static/tree/index",
 }
 
 # -- html --------------------------
