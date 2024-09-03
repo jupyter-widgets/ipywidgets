99ed88f0b67ad428debcc2d08524766109d6fd36
martinRenou <martin.renou@gmail.com>
2024-09-03 13:29:56 +0200

    Release: ipywidgets 7.8.4, widgetsnbextension 3.6.9, jupyterlab_widgets 1.1.10
    dist/ipywidgets-7.8.4-py2.py3-none-any.whl
    md5: 8007a334c6ecd3b9fabf071525debd19
    sha1: 2724c1c409a3602b897f3ebbbbbb86fd4c091ebc
    sha256: 630321dda69dddc3f7a2ed260f07aa6c120a18cc73d6bf828ed2abf43bf2c6d8
    
    dist/ipywidgets-7.8.4.tar.gz
    md5: 91b80b0a968508ed9af49646ed60ff3a
    sha1: b7e4af0f06c86c1c43ad1573ec1d4f87ebafde14
    sha256: 1268b6de789236a42507bf93f4f226e7689f7ea38807fe847ef8aec031250b9e
    
    widgetsnbextension/dist/widgetsnbextension-3.6.9-py2.py3-none-any.whl
    md5: 71bd8b09634dc32b7bd54a5325750f3f
    sha1: 771f412fc49cc48f090689c51593773bb9e7268b
    sha256: b19b369f7643b1d09b7491c200f8798e9a40c794045c902c0db348cdc3799497
    
    widgetsnbextension/dist/widgetsnbextension-3.6.9.tar.gz
    md5: 08655fb83e3b25a7670249c1e9d8df9e
    sha1: 65641f9dd2d85fe18cdc2d6ff0014d4a5b5954ab
    sha256: 0127f78e723eb6b01c2450f32388efda601c7e0d262c21a23af543c517b7fdf2
    
    jupyterlab_widgets/dist/jupyterlab_widgets-1.1.10-py3-none-any.whl
    md5: 34e0a3c337dbbff840b15e8c08827212
    sha1: cd2a29a6049b52173cbf027fee22bbed7bec3de6
    sha256: 005b64b598508ff6d573f07e8f7dc490fe4e70691f0d5d265a85d13961f3f39c
    
    jupyterlab_widgets/dist/jupyterlab_widgets-1.1.10.tar.gz
    md5: b5999bdb8a97f52c154793451c4aa746
    sha1: 0bce9ddaca69babd019e06c045b08340df02846e
    sha256: 9c52bbd830ddec397ada70fe84e75ef02a196e28aceb387afea37d23739d322b

--- setup.py
+++ setup.py
@@ -116,7 +116,7 @@ install_requires = setuptools_args['install_requires'] = [
     # only if notebook 4.x is installed in this
     # interpreter, to allow ipywidgets to be
     # installed on bare kernels.
-    'widgetsnbextension~=3.6.8'
+    'widgetsnbextension~=3.6.9'
 ]
 
 extras_require = setuptools_args['extras_require'] = {
